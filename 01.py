0e304e1244a82d1a20613d2e78f87ea40633c6fb
marcosmorandi <marcosamorandi@gmail.com>
2023-01-31 20:46:07 -0300

    01

--- 01.py
+++ 01.py
@@ -1,7 +1,7 @@
 '''
 O comando "print" exibe algo na tela, equivale a dizer "escreva".
 '''
-print('\nHello word!') # Aspas simples, escreve mensagem.
+print('\nHello word!') # Aspas simples, escreve mensagem. "\n" no começo deixa espaço acima, "\n" no final deixa espaço abaixo. 
 print(7+4) # Sem aspas, faz o cálculo.
 print('7'+'4') # Se quiser juntar mensagens, coloque o "+" entre elas.
 print('Hello', 5) # Tem horas que o "+" funciona melhor, tem horas que a "," funcionas melhor.
@@ -12,7 +12,7 @@ Começe sempre com letra minúscula.
 O "=" se lê "recebe".
 Exibindo variáveis já definidas
 '''
-print('') # "print" vazio para espaço para entre linhas. Pode se usar o "\n" no fim do "print" anterior ou no começo do próximo, dispensado essa linha com o comando vazio.
+print('') # "print" vazio para espaço para entre linhas. Pode se usar no lugar do "\n".
 nome = 'Jon'
 idade = '25 anos'
 peso = '70.5 kilos'
