521cc9bb33629aec589babe17c41ac2f58a10ef1
marcosmorandi <marcosamorandi@gmail.com>
2023-02-02 00:19:36 -0300

    d02 ok

--- d02.py
+++ d02.py
@@ -7,3 +7,7 @@ Exemplo:
 "Ano: "
 Você nasceu no dia 17 de março de 1978, correto?
 '''
+dia = input('Qual dia você nasceu? ')
+mes = input('Qual mes você nasceu? ')
+ano = input('Qual ano você nasceu? ')
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + ', correto?')
