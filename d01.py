c75a6a81cca5c7c03a9f5bb68cd1d39775586ceb
marcosmorandi <marcosamorandi@gmail.com>
2023-02-01 23:09:44 -0300

    d01 ok

--- d01.py
+++ d01.py
@@ -1,5 +1,5 @@
 '''
-Desafio 01
+Desafio 01 - Concluido!
 Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o nome digitado.
 Exemplo:
 "Qual seu nome?" 
