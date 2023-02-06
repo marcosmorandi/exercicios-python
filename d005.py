0f95154b23bb2115b5b6082711a8c22ca5f48e9f
marcosmorandi <marcosamorandi@gmail.com>
2023-02-06 08:05:53 -0300

    d005

--- d005.py
+++ d005.py
@@ -1,4 +1,9 @@
 '''
-Desafio 005
+Desafio 005 - Feito
 Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
 '''
+n = int(input('Digite um número: '))
+ns = n + 1
+na = n - 1
+
+print('O número digitado é {}, seu sucessor é {} e seu antecessor é {}.'.format(n, ns, na))
