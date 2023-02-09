60dea74a5109f158748f3b529a30c2ad6135769c
marcosmorandi <marcosamorandi@gmail.com>
2023-02-09 05:53:22 -0300

    d006

--- d006.py
+++ d006.py
@@ -1,11 +1,13 @@
 '''
-Desafio 006 - em andamento
+Desafio 006 - Feito
 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 '''
+
+import math
 
 numero = int(input('Digite um número: '))
 numero_dobro = numero * 2
 numero_triplo = numero * 3
-numero_raiz_quadrada = 0 # errado, arrumar
+numero_raiz_quadrada = math.sqrt(numero)
 
 print('O número digitado é {}, seu dobro é {}, triplo é {} e raiz quadrada é {}.'.format(numero, numero_dobro, numero_triplo, numero_raiz_quadrada))
