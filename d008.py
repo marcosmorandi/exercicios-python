f9db3b4bee533dfb5021ee910a6ed671a569b9ca
marcosmorandi <marcosamorandi@gmail.com>
2023-02-11 00:33:24 -0300

    d008

--- d008.py
+++ d008.py
@@ -2,3 +2,10 @@
 Desafio 008
 Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 '''
+
+metros = float(input('Digite uma valor em metros (use "." ao invés de ","): '))
+
+centimetros = 0 # errado, arrumar
+milimetros = 0 # errado, arrumar
+
+print('{} metros convertidos em centimetros são {} e convertido em milimetros são {}.'.format(metros, centimetros, milimetros))
