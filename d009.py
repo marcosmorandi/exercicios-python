387b6d09fbef3f192034b82b9b15668eafc540ca
marcosmorandi <marcosamorandi@gmail.com>
2023-02-12 10:22:43 -0300

    d009

--- d009.py
+++ d009.py
@@ -1,4 +1,9 @@
 '''
-Desafio 009
+Desafio 009 - Feito
 Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
 '''
+
+numero = int(input('Digite um número inteiro: '))
+
+for i in range(1, 11):
+    print(numero, "x", i, "=", numero * i)
