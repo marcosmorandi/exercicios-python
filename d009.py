5f9dd2d79b66daac1017e560a112358768f18c03
marcosmorandi <marcosamorandi@gmail.com>
2023-02-12 10:38:03 -0300

    d009

--- d009.py
+++ d009.py
@@ -5,5 +5,5 @@ Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabu
 
 numero = int(input('Digite um número inteiro: '))
 
-for i in range(1, 11):
+for i in range(1, 11): # Laço de repetição para exibir a tabuada.
     print(numero, "x", i, "=", numero * i)
