0e4631cea7ea35f0bbd8f616c80972b8211ca18f
marcosmorandi <marcosamorandi@gmail.com>
2023-02-15 06:27:18 -0300

    d012

--- d012.py
+++ d012.py
@@ -1,4 +1,10 @@
 '''
-Desafio 012
+Desafio 012 - Feito
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 '''
+
+preco = float(input('Qual o preço do produto em R$: '))
+
+novo_preco = preco - (preco * 0.05)
+
+print('O preço que antes era R$ {:.2f}, agora com 5% de desconto é R$ {:.2f}.'.format(preco, novo_preco))
