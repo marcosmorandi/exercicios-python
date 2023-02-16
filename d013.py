49935990d30853dc29db4fc707e23ae055248259
marcosmorandi <marcosamorandi@gmail.com>
2023-02-16 06:26:07 -0300

    d013

--- d013.py
+++ d013.py
@@ -1,4 +1,10 @@
 '''
-Desafio 013
+Desafio 013 - Feito
 Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 '''
+
+salario  = float(input('Qual o salário atual do funcionário em R$: '))
+
+novo_salario = salario + (salario * 0.15)
+
+print('O salário que antes era de R$ {:.2f}, agora com 15% de aumento é de R$ {:.2f}.'.format(salario, novo_salario))
