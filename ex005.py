56cf9f0d021f6a22aee253be54488f223d4a0720
marcosmorandi <marcosamorandi@gmail.com>
2023-02-17 22:39:22 -0300

    ex005

--- ex005.py
+++ ex005.py
@@ -5,4 +5,4 @@ Faça um programa que leia um número inteiro e mostre na tela seu sucessor e se
 
 n = int(input('Digite um número: '))
 print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}.'.format(n, (n-1), (n+1)))
-# Se futuramente não vai precisar da variáveis de novo, use o minimo possível para poupar memória.
+# Se futuramente não vai precisar da variável de novo, use o mínimo possível para poupar memória.
