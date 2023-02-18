b7cb21315f3b6f907eecc5bb52b5f718dea9b772
marcosmorandi <marcosamorandi@gmail.com>
2023-02-17 22:36:56 -0300

    d005

--- d005.py
+++ d005.py
@@ -5,5 +5,6 @@ Faça um programa que leia um número inteiro e mostre na tela seu sucessor e se
 numero = int(input('Digite um número: '))
 numero_sucessor = numero + 1
 numero_antecessor = numero - 1
+# Se futuramente vai precisar das variáveis de novo, declare todas as necessárias.
 
 print('O número digitado é {}, seu sucessor é {} e seu antecessor é {}.'.format(numero, numero_sucessor, numero_antecessor))
