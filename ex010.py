2f8e2d2eb5952ed06cae5803f4acd33f25c12d6a
marcosmorandi <marcosamorandi@gmail.com>
2023-02-20 19:58:22 -0300

    ex010

---
+++ ex010.py
@@ -0,0 +1,6 @@
+'''
+Exercício 010 -
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+Considere US$ 1,00 = R$ 5,22 ou a cotação atual.
+'''
+
