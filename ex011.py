25bc09d7ebda454ee36c8afd9e5faac5c77557dd
marcosmorandi <marcosamorandi@gmail.com>
2023-02-21 22:04:03 -0300

    ex011

---
+++ ex011.py
@@ -0,0 +1,5 @@
+'''
+Exercício 011
+Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pinta-lá...
+...sabendo que cada litro de tinta, pinta uma área de 2m².
+'''
