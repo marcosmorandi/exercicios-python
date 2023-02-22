3cc7933481da889384b7b51606b987ef3038321f
marcosmorandi <marcosamorandi@gmail.com>
2023-02-21 22:00:03 -0300

    d010

--- d010.py
+++ d010.py
@@ -4,12 +4,11 @@ Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre qu
 Considere US$ 1,00 = R$ 5,22 ou a cotação atual.
 '''
 
-reais = float(input('Quanto dinheiro em R$ tem disponínel: '))
-
 # Conversão de Reais para Dólar.
 # 1 / 5,2151
 # Cotação do Dólar em 12/02/2023 10h52.
 
+reais = float(input('Quanto dinheiro em R$ tem disponínel: '))
 dolar = reais / 5.2151
 
 print('Você pode comprar US$: {:.2f}'.format(dolar))
