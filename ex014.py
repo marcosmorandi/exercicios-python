f9ac615f7e2ee2342bba2ad5e93824e008830ee4
marcosmorandi <marcosamorandi@gmail.com>
2023-02-24 23:27:25 -0300

    ex014

--- ex014.py
+++ ex014.py
@@ -6,6 +6,6 @@ Escreva um programa que converta uma temperatura digitada em °C e converta para
 c = float(input('Informe a temperatura em °C: '))
 
 # Espaço entre os operadores deixa o código mais legível.
-# Pela regra de precedência, os parenteses nesse caso seriam opcionais. Gosto da legibilidade que eles dão.
+# Pela regra de precedência, os parênteses nesse caso são opcionais. Gosto da legibilidade que eles dão.
 f = ((9 * c) / 5 ) + 32 
 print('A temperatura de {}°C corresponde a {}°F'.format(c, f))
