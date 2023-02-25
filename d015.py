d9de1ae70a82defc444cf0549e3d9dd6a3865208
marcosmorandi <marcosamorandi@gmail.com>
2023-02-25 14:22:17 -0300

    d015

--- d015.py
+++ d015.py
@@ -5,11 +5,11 @@ alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço...
 a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por Km rodado.
 '''
 
-km_rodado = float(input('Quantos Km foram percorridos: '))
 dias_alugados = float(input('Por quantos dias ele foi alugado: '))
+km_rodado = float(input('Quantos Km foram percorridos: '))
 
 # calculo do preço
-preco_pagar = (0.15 * km_rodado) + (dias_alugados * 60)
+preco_pagar = (dias_alugados * 60) + (0.15 * km_rodado)
 
-print('{} Km percorridos e {} dias alugados.'.format(km_rodado, dias_alugados))
+print('{} dias alugados e {} Km percorridos.'.format(dias_alugados, km_rodado))
 print('O preço total a pagar pelo aluguel é de R$ {} .'.format(preco_pagar))
