119916c5303e6e8623018132e723629bcb2a1afa
marcosmorandi <marcosamorandi@gmail.com>
2023-02-24 23:12:07 -0300

    d014

---
+++ d014.py
@@ -0,0 +1,8 @@
+'''
+Desafio 14 - Feito
+Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
+'''
+
+temperatura_celcius = float(input('Digite a temperatura em Graus Celsius °C: '))
+temperatura_fahrenheit = (temperatura_celcius * 9 / 5) + 32
+print('A temperatura em Graus Celsius {:.0f}°C convertida em Graus Fahrenheit fica {:.1f}°F'.format(temperatura_celcius, temperatura_fahrenheit))
