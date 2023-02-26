d33f88ab8d0f85512d3d119576e44820be48dc8b
marcosmorandi <marcosamorandi@gmail.com>
2023-02-26 12:04:31 -0300

    d017

--- desafios/d017.py
+++ desafios/d017.py
@@ -6,12 +6,12 @@ retângulo, calcule e mostre o comprimento da hipotenusa.
 
 import math
 
-# leitura dos comprimentos dos catetos
+# Leitura dos comprimentos dos catetos.
 cateto_oposto = float(input('Digite o comprimento do cateto oposto: '))
 cateto_adjacente = float(input('Digite o comprimento do cateto adjacente: '))
 
-# cálculo da hipotenusa
+# Cálculo da hipotenusa.
 hipotenusa = math.sqrt(cateto_oposto ** 2  + cateto_adjacente ** 2)
 
-# exibição do resultado
+# Exibição do resultado.
 print(f'A hipotenusa do triângulo retângulo é {hipotenusa:.2f}')
