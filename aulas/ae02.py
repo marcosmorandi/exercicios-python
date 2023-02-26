c0fe55ff957a2c220e97ff7d7923a29df2a3eea6
marcosmorandi <marcosamorandi@gmail.com>
2023-02-26 12:30:07 -0300

    ae02

--- aulas/ae02.py
+++ aulas/ae02.py
@@ -1,4 +1,4 @@
-# Outra forma de formatar a saída do print
+# Outra forma de formatar a saída do "print".
 '''
 um programa Python para calcular a hipotenusa de um triângulo retângulo...
 com base nos comprimentos do cateto oposto e do cateto adjacente fornecidos...
@@ -6,14 +6,14 @@ pelo usuário:
 '''
 import math
 
-# leitura dos comprimentos dos catetos
+# Leitura dos comprimentos dos catetos.
 cateto_oposto = float(input("Digite o comprimento do cateto oposto: "))
 cateto_adjacente = float(input("Digite o comprimento do cateto adjacente: "))
 
-# cálculo da hipotenusa
+# Cálculo da hipotenusa.
 hipotenusa = math.sqrt(cateto_oposto**2 + cateto_adjacente**2)
 
-# exibição do resultado
+# Exibição do resultado.
 print(f"A hipotenusa do triângulo retângulo é {hipotenusa:.2f}")
 
 '''
