4196b6d1f425c7887f201e197e5633440778a70a
marcosmorandi <marcosamorandi@gmail.com>
2023-02-26 12:10:31 -0300

    d019

--- desafios/d019.py
+++ desafios/d019.py
@@ -3,5 +3,4 @@ Desafio 019
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro...
 Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 '''
-
 # Dica: random aqui?
