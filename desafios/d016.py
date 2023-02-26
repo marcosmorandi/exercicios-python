83c5c3bd5153cd28eec2f1e34e44c93be95e2b92
marcosmorandi <marcosamorandi@gmail.com>
2023-02-26 01:00:31 -0300

    d016

--- desafios/d016.py
+++ desafios/d016.py
@@ -1,9 +1,14 @@
 '''
-Desafio 016
+Desafio 016 - Feito
 Crie um programa que leia um número real qualquer pelo teclado e mostre na tela sua porção inteira.
 Ex:
 Digite um numero: 6.127
 O número 6.127 tem a parte inteira 6.
 '''
-
 # Dica: talvez o trunc do math resolva.
+
+from math import trunc
+
+numero = float(input('Digite um número real (com "."): '))
+inteiro = trunc(numero)
+print('Do número digitado {}, a parte inteira é {}.'.format(numero, inteiro))
