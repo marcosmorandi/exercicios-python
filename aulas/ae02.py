d2bdbe7690bbabee4cce78976ef79c140b59963e
marcosmorandi <marcosamorandi@gmail.com>
2023-02-26 11:47:15 -0300

    ae02

---
+++ aulas/ae02.py
@@ -0,0 +1,27 @@
+# Outra forma de formatar a saída do print
+'''
+um programa Python para calcular a hipotenusa de um triângulo retângulo...
+com base nos comprimentos do cateto oposto e do cateto adjacente fornecidos...
+pelo usuário:
+'''
+import math
+
+# leitura dos comprimentos dos catetos
+cateto_oposto = float(input("Digite o comprimento do cateto oposto: "))
+cateto_adjacente = float(input("Digite o comprimento do cateto adjacente: "))
+
+# cálculo da hipotenusa
+hipotenusa = math.sqrt(cateto_oposto**2 + cateto_adjacente**2)
+
+# exibição do resultado
+print(f"A hipotenusa do triângulo retângulo é {hipotenusa:.2f}")
+
+'''
+Neste programa, usamos a biblioteca math do Python...
+para calcular a raiz quadrada do quadrado dos comprimentos do cateto oposto...
+e do cateto adjacente.
+A função input é usada para solicitar ao usuário que insira os comprimentos...
+dos catetos, e a função print é usada para exibir o resultado...
+com duas casas decimais.
+Note que usamos o f-string para formatar a saída.
+'''
