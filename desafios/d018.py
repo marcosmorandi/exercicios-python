ff316213207f3c5880e44dd8c6652603828dbfd4
marcosmorandi <marcosamorandi@gmail.com>
2023-03-01 20:57:32 -0300

    d018

--- desafios/d018.py
+++ desafios/d018.py
@@ -3,8 +3,8 @@ Desafio 018 - Feito
 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno...
 e tangente desse ângulo.
 '''
-# Dica: Acho que a "math" resolve esse aqui também.
 
+# Importando toda a biblioteca "math".
 import math
 
 # Ler o ângulo em graus.
