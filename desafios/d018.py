9be9d14d1424e1eaa3c6c6969f08c25c1d4712ba
marcosmorandi <marcosamorandi@gmail.com>
2023-03-01 21:02:27 -0300

    d018

--- desafios/d018.py
+++ desafios/d018.py
@@ -4,6 +4,7 @@ Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno,
 e tangente desse ângulo.
 '''
 
+# Importando toda a biblioteca "math".
 import math
 
 # Ler o ângulo em graus.
