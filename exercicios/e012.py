345fba6a6dc6b1f8ea8934a72af4b555d22f524d
marcosmorandi <marcosamorandi@gmail.com>
2023-03-02 21:05:42 -0300

    e012

--- exercicios/e012.py
+++ exercicios/e012.py
@@ -3,6 +3,7 @@ Exercício 012 - Feito
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 '''
 
+# Forma simples de cálcular porcentagem:
 # 10*5/100 == 0.5 (5% de R$ 10,00)
 # 1500*10/100 == 150.00 (10% de R$ 1.500,00)
 # 875*15/100 == 131.25 (15% de R$ 875,00)
