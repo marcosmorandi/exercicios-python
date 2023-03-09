0b7d056ab26d759a7bf94b5bd2cfc9d338cf7be9
marcosmorandi <marcosamorandi@gmail.com>
2023-03-08 21:56:33 -0300

    e019

---
+++ exercicios/e019.py
@@ -0,0 +1,5 @@
+'''
+Exercício 019
+Um professor quer sortear um dos seus quatro alunos para apagar o quadro...
+Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+'''
