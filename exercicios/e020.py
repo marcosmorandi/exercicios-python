76845afeb888db73dc80f5c353b4584f9e7e7df9
marcosmorandi <marcosamorandi@gmail.com>
2023-03-13 22:30:33 -0300

    e020

--- exercicios/e020.py
+++ exercicios/e020.py
@@ -1,6 +1,16 @@
 '''
-Exercício 020
+Exercício 020 - Feito
 O mesmo professor do desafio anterior quer sortear a ordem de apresentação...
 de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos...
 e mostre a ordem sorteada.
 '''
+
+import random # Ou usar "from random import shuffle".
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista) # Se usado apenas o "shuflle", apagar o "randon." aqui.
+print('A ordem de apresentação será ')
+print(lista)
