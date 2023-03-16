fe5269ad984c2a673d5d1b3b768d60ae12bbf659
marcosmorandi <marcosamorandi@gmail.com>
2023-03-15 22:50:39 -0300

    d021

--- desafios/d021.py
+++ desafios/d021.py
@@ -2,3 +2,10 @@
 Desafio 021
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
+
+from playsound import playsound
+
+# caminho do arquivo de áudio
+path = 'C:\Users\marco\Documents\VSCode\Projetos\exercicios-python\desafios\d021.mp3'
+# reproduz o arquivo de áudio
+playsound(path)
