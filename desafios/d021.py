9af50b21e30ea7b296be8568da87461db3663b55
marcosmorandi <marcosamorandi@gmail.com>
2023-03-15 22:57:30 -0300

    d021

--- desafios/d021.py
+++ desafios/d021.py
@@ -3,9 +3,20 @@ Desafio 021
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
 
-from playsound import playsound
+import pygame
 
-# caminho do arquivo de áudio
-path = 'C:\Users\marco\Documents\VSCode\Projetos\exercicios-python\desafios\d021.mp3'
+# inicializa o pygame
+pygame.init()
+
+# carrega o arquivo de áudio
+pygame.mixer.music.load("d021.mp3")
+
 # reproduz o arquivo de áudio
-playsound(path)
+pygame.mixer.music.play()
+
+# espera a reprodução do áudio terminar
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+# para a reprodução do áudio
+pygame.mixer.music.stop()
