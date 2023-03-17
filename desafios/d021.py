23ed2b9fbc62eea0e18ceb1090c22a6f7d9af2d3
marcosmorandi <marcosamorandi@gmail.com>
2023-03-16 21:59:42 -0300

    d021

--- desafios/d021.py
+++ desafios/d021.py
@@ -3,20 +3,4 @@ Desafio 021
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
 
-import pygame
-
-# inicializa o pygame
-pygame.init()
-
-# carrega o arquivo de áudio
-pygame.mixer.music.load("d021.mp3")
-
-# reproduz o arquivo de áudio
-pygame.mixer.music.play()
-
-# espera a reprodução do áudio terminar
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
-
-# para a reprodução do áudio
-pygame.mixer.music.stop()
+# Usar o "playsound". Funcionou no VSCode Portable .MP3 com 128kbps e 320kbps.
