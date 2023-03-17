81e015e345645cf1dab4f8c2a01a609a2b35686c
marcosmorandi <marcosamorandi@gmail.com>
2023-03-16 22:08:23 -0300

    d021

--- desafios/d021.py
+++ desafios/d021.py
@@ -4,4 +4,4 @@ Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
 
 # Usar o "playsound". Funcionou no VSCode Portable .MP3 com 128kbps e 320kbps.
-# Cuidar a quantidade e tipo de caracteres no nome.
+# Cuidar a quantidade e tipo de caracteres no nome. Testar com calma.
