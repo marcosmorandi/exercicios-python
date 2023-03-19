db648f983266203e80b334c00f048d0f050a9388
marcosmorandi <marcosamorandi@gmail.com>
2023-03-19 17:20:45 -0300

    e021

--- exercicios/e021.py
+++ exercicios/e021.py
@@ -1,9 +1,20 @@
 '''
-Exercício 021
+Exercício 021 - Feito
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
 
 # Usar o "Pygame".
-from playsound import playsound
-path = ".\media\Bibio-Lovers_Carvings.mp3"
-playsound(path)
+
+# Importando o "Pygame".
+import pygame
+
+# Inicializando o "mixer" do "Pygame". Tem que ser nessa ordem, primeiro "mixer" depois "Pygame".
+pygame.mixer.init()
+
+# Inicializando o "Pygame".
+pygame.init()
+
+# No VSCode usar o ".\" para indicar o caminho do arquivo ".mp3".
+pygame.mixer.music.load('.\media\Bibio-Lovers_Carvings.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
