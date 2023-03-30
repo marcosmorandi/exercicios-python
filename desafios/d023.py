8e0e5c08a80a00def51209141cafcc9a57f8ec05
marcosmorandi <marcosamorandi@gmail.com>
2023-03-30 19:23:33 -0300

    d023

--- desafios/d023.py
+++ desafios/d023.py
@@ -1,1 +1,10 @@
-# Desafio 023
+'''
+Desafio 023
+Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
+Ex:
+Digite um número: 1834
+Unidade: 4
+Dezena: 3
+Centenas: 8
+Milhar: 1
+'''
