b54314242d912ab6acfb95e9cb9a17fb0a34f584
marcosmorandi <marcosamorandi@gmail.com>
2023-04-02 17:53:21 -0300

    d022

--- desafios/d022.py
+++ desafios/d022.py
@@ -7,11 +7,11 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 * Quantas letras tem o primeiro nome.
 '''
 nome_completo = input('Digite o nome completo: ')
-print(nome_completo.upper()) # Letras maiúsculas. - ok
-print(nome_completo.lower()) # Letras minúsculas. - ok
+print(nome_completo.upper()) # Letras maiúsculas.
+print(nome_completo.lower()) # Letras minúsculas.
 
-sem_espacos = nome_completo.replace(' ', '') # Quantas letras ao todo (sem considerar espaços). - ok
+sem_espacos = nome_completo.replace(' ', '') # Quantas letras ao todo (sem considerar espaços).
 print(len(sem_espacos))
 
-divide_nomes = nome_completo.split() # Quantas letras tem o primeiro nome. - ok
+divide_nomes = nome_completo.split() # Quantas letras tem o primeiro nome.
 print(len(divide_nomes[0]))
