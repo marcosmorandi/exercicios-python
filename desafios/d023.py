f188622aa9d755653b7024a747941bd248e1365e
marcosmorandi <marcosamorandi@gmail.com>
2023-04-03 19:28:13 -0300

    d023

--- desafios/d023.py
+++ desafios/d023.py
@@ -1,5 +1,5 @@
 '''
-Desafio 023
+Desafio 023 - Feito
 Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
 Ex:
 Digite um número: 1834
@@ -8,3 +8,8 @@ Dezena: 3
 Centenas: 8
 Milhar: 1
 '''
+numero = input(str('Digite um número de 0 a 9999: '))
+print(f'Unidade: {numero[3]}')
+print(f'Dezena: {numero[2]}')
+print(f'Centena: {numero[1]}')
+print(f'Milhar: {numero[0]}')
