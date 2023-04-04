f87d8203e8c5a3889776afa6fc989c7761de596f
marcosmorandi <marcosamorandi@gmail.com>
2023-04-04 19:44:39 -0300

    d024

--- desafios/d024.py
+++ desafios/d024.py
@@ -1,4 +1,12 @@
 '''
-Desafio 024
+Desafio 024 - Feito
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 '''
+cidade = input(str('Digite o nome de uma cidade: '))
+
+if ('Santo' in cidade) == True:
+    print('Tem o nome "Santo" nessa cidade!')
+elif ('SANTO' in cidade) == True:
+    print('Tem o nome "Santo" nessa cidade!')
+else:
+    print('Não tem o nome "Santo" nessa cidade!')
