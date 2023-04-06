b93d90d0c76816c6ba225e5dfa64525563320b15
marcosmorandi <marcosamorandi@gmail.com>
2023-04-06 19:21:41 -0300

    d025

--- desafios/d025.py
+++ desafios/d025.py
@@ -1,4 +1,13 @@
 '''
-Desafio 025
+Desafio 025 - Feito
 Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 '''
+
+nome = input(str('Digite seu nome: '))
+
+nome.split()
+
+if ('Silva' in nome.split()) or ('silva' in nome.split()) or ('SILVA' in nome.split()):
+    print('Tem "Silva" no nome!')
+else:
+    print('Não tem "Silva" no nome!')
