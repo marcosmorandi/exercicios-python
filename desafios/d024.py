1d586aaf729b56c7b80673a222cfe13122f35168
marcosmorandi <marcosamorandi@gmail.com>
2023-04-12 15:41:52 -0300

    d024

--- desafios/d024.py
+++ desafios/d024.py
@@ -2,6 +2,7 @@
 Desafio 024 - Feito
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 '''
+
 cidade = input(str('Digite o nome de uma cidade: '))
 
 cidade.split()
