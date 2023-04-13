a4fc5dd96ca6c3dd2eabb80dbda01ae00da9a4fe
marcosmorandi <marcosamorandi@gmail.com>
2023-04-13 16:12:20 -0300

    e022

--- exercicios/e022.py
+++ exercicios/e022.py
@@ -1,1 +1,15 @@
-# Exercício 022
+'''
+Exercício 022
+Crie um programa que leia o nome completo de uma pessoa e mostre:
+* O nome com todas as letras maiúsculas.
+* O nome com todas as letras minúsculas.
+* Quantas letras ao todo (sem considerar espaços).
+* Quantas letras tem o primeiro nome.
+'''
+
+nome = str(input('Digite o nome completo: ')).strip()
+print('Analisndo o nome...')
+print(f'O nome em maiúsculas é {(nome.upper())}.')
+print(f'O nome em minúsculas é {(nome.lower())}.')
+print(f'O tem ao todo {(len(nome) - nome.count(" "))} letras.')
+print(f'O primeiro nome tem {(nome.find(" "))} letras.')
