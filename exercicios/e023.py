a25dd61798212e72ecbcc77d9528547d88eb7720
marcosmorandi <marcosamorandi@gmail.com>
2023-04-15 20:14:51 -0300

    e023

--- exercicios/e023.py
+++ exercicios/e023.py
@@ -1,1 +1,21 @@
-# Exercício 023
+'''
+Exercício 023 - Feito
+Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
+Ex:
+Digite um número: 1834
+Unidade: 4
+Dezena: 3
+Centenas: 8
+Milhar: 1
+'''
+
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o numero {num}')
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
