066a92c875a65b6309798a9cb1730f49f0cbc504
marcosmorandi <marcosamorandi@gmail.com>
2023-04-15 20:15:50 -0300

    d023

--- desafios/d023.py
+++ desafios/d023.py
@@ -9,8 +9,13 @@ Centenas: 8
 Milhar: 1
 '''
 
-numero = input(str('Digite um número de 0 a 9999: '))
-print(f'Unidade: {numero[3]}')
-print(f'Dezena: {numero[2]}')
-print(f'Centena: {numero[1]}')
-print(f'Milhar: {numero[0]}')
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o numero {num}')
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
