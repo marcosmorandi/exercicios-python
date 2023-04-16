dff55352f41043c85edc65e8565fbf6a698a881e
marcosmorandi <marcosamorandi@gmail.com>
2023-04-16 20:22:52 -0300

    e024

--- exercicios/e024.py
+++ exercicios/e024.py
@@ -3,5 +3,5 @@ Exercício 024 - Feito
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 '''
 
-cid = str(input('Em que cidade você nasceu? ')).strip()
+cid = str(input('Em que cidade você nasceu? ')).strip() # ".strip" remove espaços vazios antes e depois da string.
 print(cid[:5].upper() == 'SANTO') # Independente de como o usuário digitar(maiúscula, minúscula, com espaço), ele joga para maiusculo e verifica a palavra. 
