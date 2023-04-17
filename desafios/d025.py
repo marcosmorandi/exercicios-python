aeef883cf62cfb9a0d70a015f6db61a7100ec8bb
marcosmorandi <marcosamorandi@gmail.com>
2023-04-17 15:15:23 -0300

    d025

--- desafios/d025.py
+++ desafios/d025.py
@@ -3,11 +3,9 @@ Desafio 025 - Feito
 Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 '''
 
-nome = input(str('Digite seu nome: '))
+nome = str(input('Digite seu nome: ')).strip() # Remove espaços em branco antes e depois da string.
 
-nome.split()
-
-if ('Silva' in nome.split()) or ('silva' in nome.split()) or ('SILVA' in nome.split()):
-    print('Tem "Silva" no nome!')
+if ('silva' in nome.lower()) == True: # Independente de como o usuário digitar, deixa tudo em minúsculo e procura a palavra.
+    print('Tem "Silva" no nome.')
 else:
-    print('Não tem "Silva" no nome!')
+    print('Não tem "Silva" no nome.')
