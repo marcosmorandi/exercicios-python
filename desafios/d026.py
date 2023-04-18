fb9436c26cc37d66daced1e5d2587784f278af5f
marcosmorandi <marcosamorandi@gmail.com>
2023-04-18 14:04:50 -0300

    d026

--- desafios/d026.py
+++ desafios/d026.py
@@ -6,14 +6,7 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 * Em que posição ela aparece a última vez.
 '''
 
-frase = input(str('Digite uma frase: '))
-
-conta1 = frase.count('A')
-conta2 = frase.count('a')
-aparece_vezes = conta1 + conta2 # Certo
-aparece_primeiro = frase.find('a') # Errado
-aparece_ultimo = frase.find('a') # Errado
-
-print(f'''Aparece a letra "A" ou "a" {aparece_vezes} veze(s).
-A primeira vez na posição {aparece_primeiro}.
-A ultima vez na posição {aparece_ultimo}''')
+frase = str(input('Digite uma frase: ')).lower().strip() # Independente de como o usuário digitar, "lower" joga tudo para minúsculo. "strip" remove os espaços antes e depois da string.
+print(f'A letra "A" aparece {(frase.count("a"))} vezes na frase.') # Não tente fazer o programa inteiro de uma vez, faça por partes e vá testando.
+print(f'A primeira letra "A" apareceu na posição {(frase.find("a")+1)}.') # "+1" pois para o Pyhon é posição "0", mas para pessoas faz mais sentido posição "1". Então 0 + 1.
+print(f'A última letra "A" apareceu na posição {(frase.rfind("a")+1)}.') # "rfind" é para iniciar a procura pelo lado direito, do final em direção ao começo.
