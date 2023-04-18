2cfdb769e70ecbb79b7861dbee81fab6cc45600c
marcosmorandi <marcosamorandi@gmail.com>
2023-04-18 14:06:36 -0300

    d026

--- desafios/d026.py
+++ desafios/d026.py
@@ -1,5 +1,5 @@
 '''
-Desafio 026 - Não feito, ver aula.
+Desafio 026 - Feito
 Faça um programa que leia uma frase pelo teclado e mostre:
 * Quantos vezes aparece a letra "A".
 * Em que posição ela aparece a primeira vez.
