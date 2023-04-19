ee0f9a22ca0614a169b9e8412399dd657a11a17c
marcosmorandi <marcosamorandi@gmail.com>
2023-04-19 14:39:06 -0300

    d024

--- desafios/d024.py
+++ desafios/d024.py
@@ -3,9 +3,7 @@ Desafio 024 - Feito
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 '''
 
-cidade = input(str('Digite o nome de uma cidade: ')).strip() # ".strip" remove espaços vazios antes e depois da string.
-
-cidade = cidade.split() # ".split" divide a string onde tem espaço, recebendo indexação 0,1,2,3...
+cidade = str(input('Digite o nome de uma cidade: ')).strip().split() # ".strip" remove espaços vazios antes e depois da string, ".split" divide a string onde tem espaços no meio, com nova indexação 0,1,2...
 
 # Sempre tente antecipar qualquer possível erro do usuário e corrigir.
 if (cidade[0].lower()) == 'santo': # Independente de como o usuário digitar(minúscula, maiúscula, misturado), ele joga para minúscula e verifica a palavra na posição "0" da string.
