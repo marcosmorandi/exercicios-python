8202b9281f2ae0242e936252a33312b47fb22a8b
marcosmorandi <marcosamorandi@gmail.com>
2023-04-19 15:44:39 -0300

    e027

--- exercicios/e027.py
+++ exercicios/e027.py
@@ -1,5 +1,5 @@
 '''
-Exercício 027
+Exercício 027 - Feito
 Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 Ex: Ana Maria de Souza
 Primeiro = Ana
