568f37e138137ec5774b3d44c292de414ea5e9bf
marcosmorandi <marcosamorandi@gmail.com>
2023-04-19 14:42:07 -0300

    d026

--- desafios/d026.py
+++ desafios/d026.py
@@ -8,5 +8,5 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 
 frase = str(input('Digite uma frase: ')).lower().strip() # Independente de como o usuário digitar, "lower" joga tudo para minúsculo. "strip" remove os espaços antes e depois da string.
 print(f'A letra "A" aparece {(frase.count("a"))} vezes na frase.') # Não tente fazer o programa inteiro de uma vez, faça por partes e vá testando.
-print(f'A primeira letra "A" apareceu na posição {(frase.find("a")+1)}.') # "+1" pois para o Pyhon é posição "0", mas para pessoas faz mais sentido posição "1". Então 0 + 1.
-print(f'A última letra "A" apareceu na posição {(frase.rfind("a")+1)}.') # "rfind" é para iniciar a procura pelo lado direito, do final em direção ao começo.
+print(f'A primeira letra "A" aparece na posição {(frase.find("a")+1)}.') # "+1" pois para o Pyhon é posição "0", mas para pessoas faz mais sentido posição "1". Então 0 + 1.
+print(f'A última letra "A" aparece na posição {(frase.rfind("a")+1)}.') # "rfind" é para iniciar a procura pelo lado direito, do final em direção ao começo.
