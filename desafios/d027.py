8c99c967419b275aa771b0ec53ad19f46a65765b
marcosmorandi <marcosamorandi@gmail.com>
2023-04-19 16:03:43 -0300

    d027

--- desafios/d027.py
+++ desafios/d027.py
@@ -1,16 +1,14 @@
 '''
-Desafio 027 - Incorreto, ver aula
+Desafio 027 - (Tentar refazer com o strip e split juntos)
 Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 Ex: Ana Maria de Souza
 Primeiro = Ana
 Último = Souza
 '''
 
-nome_completo = input(str('Digite o nome completo: '))
+nome_inteiro = str(input('Digite um nome: ')).strip() # Tira espaços vazios antes e depois da string.
+nome_separado = nome_inteiro.split() # Separa o nome e da nova indexação para a string.
+print(f'O primeiro nome é {(nome_separado[0])}.') # Primeira posição do nome.
+print(f'O último nome é {(nome_separado[len(nome_separado)-1])}.')
 
-primeiro_nome = nome_completo.split()
-
-ultimo_nome = nome_completo.split()
-
-print(f'Primeiro nome: {primeiro_nome[0]}')
-print(f'Ultimo nome: {ultimo_nome[:]}')
+# ver comentários aqui para entender o -1 https://www.youtube.com/watch?v=SifYYsXhLM8&t=404s
