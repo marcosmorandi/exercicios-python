724a9ac35518eae42bd74a2a251d3d9df28011b5
marcosmorandi <marcosamorandi@gmail.com>
2023-04-19 14:42:41 -0300

    e026

--- exercicios/e026.py
+++ exercicios/e026.py
@@ -9,4 +9,4 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 frase = str(input('Digite uma frase: ')).lower().strip() # Pode usar o "lower" e "strip" simultaneamente.
 print(f'A letra "A" aparece {(frase.count("a"))} vezes na frase.')
 print(f'A primeira letra "A" aparece na posição {(frase.find("a")+1)}.')
-print(f'A última letra "A" aparece na posiçao {(frase.rfind("a")+1)}')
+print(f'A última letra "A" aparece na posição {(frase.rfind("a")+1)}')
