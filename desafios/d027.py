39e52132f5bca39ab630a0adf98b80be5402d11d
marcosmorandi <marcosamorandi@gmail.com>
2023-04-20 16:16:03 -0300

    d027

--- desafios/d027.py
+++ desafios/d027.py
@@ -9,6 +9,29 @@ Primeiro = Ana
 nome_inteiro = str(input('Digite um nome: ')).strip() # Tira espaços vazios antes e depois da string.
 nome_separado = nome_inteiro.split() # Separa o nome e da nova indexação para a string.
 print(f'O primeiro nome é {(nome_separado[0])}.') # Primeira posição do nome.
-print(f'O último nome é {(nome_separado[len(nome_separado)-1])}.')
+print(f'O último nome é {(nome_separado[len(nome_separado)-1])}.') # Explicação abaixo.
 
-# ver comentários aqui para entender o -1 https://www.youtube.com/watch?v=SifYYsXhLM8&t=404s
+'''
+Na linha 12, O "len" percorre a string numerando de 0 até + 1 após o final, o "-1" remove o último espaço vazio e exibe a posição ou mostra a última letra de fato.
+
+Ex:
+-----
+nome = str(input('Digite um nome: ')).strip()
+print(f'A última posição do nome é {(len(nome))}.')
+print(f'E é a letra {(nome[len(nome)])}.')
+
+Digite um nome: ana
+              # 0123
+A última posição do nome é 3.
+!!! IndexError: string index out of range !!! # Não tem letra na posição 3.
+-----
+nome = str(input('Digite um nome: ')).strip()
+print(f'A última posição do nome é {(len(nome))}.')
+print(f'E é a letra {(nome[len(nome)-1])}.')
+
+Digite um nome: ana
+              # 0123
+A última posição do nome é 3.
+E é a letra a. # 0123 -1 =2 que é a letra "a" na posição "2".
+-----
+'''
