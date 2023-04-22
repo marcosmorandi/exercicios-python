6a8b21d0b4e5a453dae89cfdf65419eac2ab2c70
marcosmorandi <marcosamorandi@gmail.com>
2023-04-22 18:53:37 -0300

    a010t

---
+++ aulas/a010t.py
@@ -0,0 +1,4 @@
+# Estrutura condicional.
+# Todo método tem parenteses no final.
+# "if:" = se; "else:" = senão.
+# Todo comando colado do lado esquerdo da tela será executado sempre. Todo comando com identação (para direita) pode ser executado ou não.
