0afbcd36e6126db234d252bd36adbed449544573
marcosmorandi <marcosamorandi@gmail.com>
2023-05-01 16:53:11 -0300

    d028

--- desafios/d028.py
+++ desafios/d028.py
@@ -7,7 +7,7 @@ O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 import random # Biblioteca que gera o número aleatório.
 
-numero_aleatorio = random.randint(0,5) # A variável é definida usando o método "randint" da biblioteca random, que gera um número inteiro aleatório entre 0 e 5.
+numero_aleatorio = random.randint(0,5) # A variável é definida usando o método "randint" da biblioteca "random", que gera um número inteiro aleatório entre 0 e 5.
 
 palpite = int(input('Tente advinhar o número gerado pelo computador, de 0 a 5: ')) # Lê o palpite do usuário.
 
