6c4356fc57741d27c59e7e0e10ccd850aae7e350
marcosmorandi <marcosamorandi@gmail.com>
2023-05-01 16:51:45 -0300

    d028

--- desafios/d028.py
+++ desafios/d028.py
@@ -1,6 +1,17 @@
 '''
-Desafio 028
+Desafio 028 - Feito
 Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça
 para o usuário tentar descobrir qual foi o número escolhido pelo computador.
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 '''
+
+import random # Biblioteca que gera o número aleatório.
+
+numero_aleatorio = random.randint(0,5) # A variável é definida usando o método "randint" da biblioteca random, que gera um número inteiro aleatório entre 0 e 5.
+
+palpite = int(input('Tente advinhar o número gerado pelo computador, de 0 a 5: ')) # Lê o palpite do usuário.
+
+if palpite == numero_aleatorio: # Se o palpite for igual o numero aleatório:
+    print(f'Número gerado {numero_aleatorio}, seu palpite {palpite}. Paranéns, acertou!')
+else: # Senão:
+    print(f'Número gerado {numero_aleatorio}, seu palpite {palpite}. Que pena, errou!')
