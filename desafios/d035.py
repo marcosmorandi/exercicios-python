eb22b4cf4029a2baaf6dafbc3d660d620ebcc10e
marcosmorandi <marcosamorandi@gmail.com>
2023-04-30 23:14:16 -0300

    d035

---
+++ desafios/d035.py
@@ -0,0 +1,6 @@
+'''
+Desafio 035
+Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem
+ou não formar um triângulo.
+'''
+# Pesquise o principio matemático que diz se 3 retas podem ou não formar um triângulo.
