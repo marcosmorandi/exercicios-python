b4ec667e63be35b5d406bf3196127d231a17559d
marcosmorandi <marcosamorandi@gmail.com>
2023-04-30 22:49:48 -0300

    d027

--- desafios/d027.py
+++ desafios/d027.py
@@ -1,5 +1,5 @@
 '''
-Desafio 027 - (Tentar refazer com o strip e split juntos)
+Desafio 027 - Feito
 Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 Ex: Ana Maria de Souza
 Primeiro = Ana
