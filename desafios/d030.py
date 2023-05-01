dbfd1882006ea11c23d0fe999a470ba33741cc5b
marcosmorandi <marcosamorandi@gmail.com>
2023-05-01 17:29:33 -0300

    d030

--- desafios/d030.py
+++ desafios/d030.py
@@ -1,4 +1,14 @@
 '''
-Desafo 030
+Desafio 030 - Feito
 Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou Ímpar.
 '''
+
+# Considera-se um número par quando o resto da divisão por 2 é igual a 0.
+# Considera-se um número ímpar quando o resto da divisão por 2 é diferente de 0.
+
+numero = int(input('Digite um número inteiro: '))
+
+if (numero % 2) == 0:
+    print('Número par!')
+else:
+    print('Número ímpar!')
