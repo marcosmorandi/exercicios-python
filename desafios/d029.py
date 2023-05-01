7edd613a6f9360ce81f6e09de431d310a7e06f1a
marcosmorandi <marcosamorandi@gmail.com>
2023-05-01 17:00:48 -0300

    d029

--- desafios/d029.py
+++ desafios/d029.py
@@ -4,3 +4,10 @@ Escreva um programa que leia a velocidade de um carro.
 Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
 A multa vai custar R$ 7,00 por cada Km acima do limite.
 '''
+
+velocidade = int(input('Qual a velocidade do carro: '))
+
+if velocidade <= 80:
+    print(f'Velocidade {velocidade}Km/h, limite 80km/h, dentro do limite!')
+else:
+    print(f'Velocidade {velocidade}Km/h, limite 80km/h, fora do limite, multado!')
