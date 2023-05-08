0abdf931ecc83f4bec74d4c0c2fd9123679fbe8c
marcosmorandi <marcosamorandi@gmail.com>
2023-05-08 00:42:03 -0300

    e030

--- exercicios/e030.py
+++ exercicios/e030.py
@@ -1,4 +1,11 @@
 '''
-Exercício 030
+Exercício 030 - Feito
 Crie um programa que leia um número inteiro e mostre na tela se ele é Par ou Ímpar.
 '''
+
+numero = int(input('Me diga um número qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print(f'O número {numero} é Par!')
+else:
+    print(f'O número {numero} é Ímpar!')
