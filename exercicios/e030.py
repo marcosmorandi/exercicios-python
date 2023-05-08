a61f4ac3bf51cf98e9e05c650bd43194f64b9151
marcosmorandi <marcosamorandi@gmail.com>
2023-05-08 00:36:36 -0300

    e030

--- exercicios/e030.py
+++ exercicios/e030.py
@@ -1,5 +1,4 @@
 '''
 Exercício 030
-Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou Ímpar.
+Crie um programa que leia um número inteiro e mostre na tela se ele é Par ou Ímpar.
 '''
-
