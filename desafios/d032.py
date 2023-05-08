2c8e1936694a7db584de41cff92c9795b5fcfd8a
marcosmorandi <marcosamorandi@gmail.com>
2023-05-07 21:49:28 -0300

    d032

--- desafios/d032.py
+++ desafios/d032.py
@@ -1,5 +1,11 @@
 '''
-Desafio 032
+Desafio 032 - Feito
 Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 '''
+
 ano = int(input('Digite o ano para saber se ele é ou não bissexto: '))
+
+if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+    print(f'O ano {ano} é bissexto.')
+else:
+    print(f'O ano {ano} é não é bissexto.')
