d94a345a2b98d8106f86285e63e1035c8971dc0e
marcosmorandi <marcosamorandi@gmail.com>
2023-05-22 00:15:16 -0300

    d032

--- desafios/d032.py
+++ desafios/d032.py
@@ -8,4 +8,4 @@ ano = int(input('Digite o ano para saber se ele é ou não bissexto: '))
 if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
     print(f'O ano {ano} é bissexto.')
 else:
-    print(f'O ano {ano} é não é bissexto.')
+    print(f'O ano {ano} não é bissexto.')
