1345a361c360c0810a1ba737044d58efd67937c0
marcosmorandi <marcosamorandi@gmail.com>
2023-05-27 21:29:13 -0300

    d035

--- desafios/d035.py
+++ desafios/d035.py
@@ -1,16 +1,16 @@
 '''
-Desafio 035 - Não parece correto
+Desafio 035 - Feito
 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem
 ou não formar um triângulo.
 '''
 # Pesquise o principio matemático que diz se 3 retas podem ou não formar um triângulo.
-# A regra é a seguinte: A soma de dois lados é sempre menor que o terceiro lado.
+# A regra é a seguinte: Cada um dos segumimentos tem que ser menor que a soma do comprimento dos outros dois.
 
-reta_1 = int(input('Digite o tamanho da primeira reta: '))
-reta_2 = int(input('Digite o tamanho da segunda reta: '))
-reta_3 = int(input('Digite o tamanho da terceira reta: '))
+reta_1 = float(input('Digite o tamanho da primeira reta: '))
+reta_2 = float(input('Digite o tamanho da segunda reta: '))
+reta_3 = float(input('Digite o tamanho da terceira reta: '))
 
-if ((reta_1 + reta_2) < reta_3) or ((reta_1 + reta_3) < reta_2) or ((reta_2 + reta_3) < reta_1):
+if ((reta_1 + reta_2) > reta_3) and ((reta_1 + reta_3) > reta_2) and ((reta_2 + reta_3) > reta_1):
     print('Essas medidas podem formar um triângulo.')
 else:
     print('Essas medidas não podem formar um triângulo.')
