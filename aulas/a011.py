a90df1ee587ce1fa3b2fd3c7dd1cbd371c06c233
marcosmorandi <marcosamorandi@gmail.com>
2023-05-28 23:29:54 -0300

    a011

---
+++ aulas/a011.py
@@ -0,0 +1,37 @@
+# Cores no Terminal
+
+'''
+Cores ANSI escape sequence
+\033[0;33;44m
+\033[(estilo);(cor do texto);(cor do fundo)m
+"\033[" abre, "m" fecha.
+Não tem necessidade de definir os 3 parâmetros, se tiver mais de um separe com ";"
+Não esqueça de fechar com "\033[m" para não colorir informações posteriores.
+
+Estilos:
+0 - Nenhum
+1 - Negrito
+4 - Sublinhado
+7 - Negativo, inverte texto e fundo.
+
+Cor do Texto:
+30 - Branco
+31 - Vermelho
+32 - Verde
+33 - Amarelo
+34 - Azul
+35 - Roxo
+36 - Ciano
+37 - Cinza
+
+Cor do Fundo:
+40 - Branco
+41 - Vermelho
+42 - Verde
+43 - Amarelo
+44 - Azul
+45 - Roxo
+46 - Ciano
+47 - Cinza
+'''
+
