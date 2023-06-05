8fa7498d5e4b6d2ea268c35b058c27e3520b8989
marcosmorandi <marcosamorandi@gmail.com>
2023-06-04 23:38:30 -0300

    a012

---
+++ aulas/a012.py
@@ -0,0 +1,12 @@
+# Condições Aninhadas
+
+# Dentro de um "if" pode usar quantos "elif" quiser, "else" usa-se uma ou nenhuma vez.
+
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome comum!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil.')
+else:
+    print('Sorte sua não se chamar Gustavo.')
+print(f'Tenha um bom dia {nome}.')
