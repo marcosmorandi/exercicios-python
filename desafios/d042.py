3ef4a033b3980f28be0851fa0ffbc33699c3d54a
marcosmorandi <marcosamorandi@gmail.com>
2023-06-05 00:38:57 -0300

    d042

---
+++ desafios/d042.py
@@ -0,0 +1,9 @@
+'''
+Desafio 042
+Refaça o desafio 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo
+será formado:
+- Equilátero: Todos os lado iguais
+- Isósceles: Dois lados iguas
+- Escaleno: Todos os lados diferentes
+'''
+
