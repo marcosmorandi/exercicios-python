738472eb0b09c94259f9c2d350e2d110e1c6dc9d
marcosmorandi <marcosamorandi@gmail.com>
2023-06-05 00:12:19 -0300

    a012

--- aulas/a012.py
+++ aulas/a012.py
@@ -7,6 +7,8 @@ if nome == 'Gustavo':
     print('Que nome comum!')
 elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
     print('Seu nome é bem popular no Brasil.')
+elif nome in 'Ana Claudia Jessica Juliana':
+    print('Belo nome feminino.')
 else:
     print('Sorte sua não se chamar Gustavo.')
 print(f'Tenha um bom dia {nome}.')
