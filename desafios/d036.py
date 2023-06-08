fe1c68b634a990eb14615457a233cf018ebe3cd9
marcosmorandi <marcosamorandi@gmail.com>
2023-06-08 17:38:46 -0300

    d036

--- desafios/d036.py
+++ desafios/d036.py
@@ -1,15 +1,22 @@
 '''
-Desafio 036
+Desafio 036 - Feito
 Escreva um programa para aprovar o empréstimo bancário para compra de uma casa.
 O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o
 empréstimo será negado. Desconsidere os juros.
 '''
 
-valor_casa = float(input('Qual o valor da casa? '))
-salario = float(input('Qual o salário do comprador? '))
-anos_pagando = int(input('Em quantos anos vai pagar? '))
+valor_casa = float(input('Qual o valor da casa: '))
+salario = float(input('Qual o salário do comprador: '))
+anos_pagando = int(input('Em quantos anos vai pagar: '))
 
-prestacao = valor_casa / (anos_pagando + 11)
+prestacao_anual = valor_casa / anos_pagando
+prestacao_mensal = prestacao_anual / 12
 
-print(f'Prestação Mensal de R$ {prestacao}.')
+if prestacao_mensal > (salario * 30 / 100):
+    print('Emprestimo negado!')
+    print('O valor da prestação excede 30% do salário.')
+    print(f'Prestação mensal: R$ {prestacao_mensal:.2f}')
+else:
+    print('Emprestimo aprovado!')
+    print(f'Prestação mensal: R$ {prestacao_mensal:.2f}')
