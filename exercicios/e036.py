68b47ac9df366d9302d9611f59f877b91d76e2d8
marcosmorandi <marcosamorandi@gmail.com>
2023-06-11 23:13:21 -0300

    e036

--- exercicios/e036.py
+++ exercicios/e036.py
@@ -6,3 +6,16 @@ Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do 
 empréstimo será negado. Desconsidere os juros.
 '''
 
+casa = float(input('Valor da casa: R$ '))
+salario = float(input('Salário do comprador: R$ '))
+anos = int(input('Quantos anos de financiamento? '))
+
+prestacao = casa / (anos * 12)
+minimo = salario * 30 / 100 # 30% do salário.
+
+print(f'Para pagar uma casa de R$ {casa:.2f} em {anos} anos a prestação será de {prestacao:.2f}')
+
+if prestacao <= minimo:
+    print('Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
