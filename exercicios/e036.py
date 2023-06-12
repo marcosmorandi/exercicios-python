61ad52758132a5ac89697fd3d0af77515f94b290
marcosmorandi <marcosamorandi@gmail.com>
2023-06-11 23:13:45 -0300

    e036

--- exercicios/e036.py
+++ exercicios/e036.py
@@ -1,5 +1,5 @@
 '''
-Exercício 036
+Exercício 036 - Feito
 Escreva um programa para aprovar o empréstimo bancário para compra de uma casa.
 O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o
