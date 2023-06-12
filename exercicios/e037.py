a84d4a73b4148b3f30f841cabb61aecac97fc43a
marcosmorandi <marcosamorandi@gmail.com>
2023-06-12 00:44:22 -0300

    e037

--- exercicios/e037.py
+++ exercicios/e037.py
@@ -19,6 +19,6 @@ if opcao == 1:
 elif opcao == 2:
     print(f'{num} convertido para OCTAL é igual a {oct(num)[2:]}.') # "[2:]" técnica de fatiamento de string, a posição 0 e 1 não quero que apareça, mostra da 3ª posição ao final.
 elif opcao == 3:
-    print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}.') # Sem o fatiamento ele imprime dois caracteres referente ao tipo de conversão. 
+    print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}.') # Sem o fatiamento ele imprime dois caracteres referente ao tipo de conversão.
 else:
     print('Opção inválida! Tente novamente.')
