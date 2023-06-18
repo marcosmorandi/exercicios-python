40e281faf38086f9c4600b23d228fbb4c45cc703
marcosmorandi <marcosamorandi@gmail.com>
2023-06-18 03:28:04 -0300

    d040

--- desafios/d040.py
+++ desafios/d040.py
@@ -1,5 +1,5 @@
 '''
-Desafio 040
+Desafio 040 - Feito
 Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando um mensagem no
 final, de acordo com a média atingida:
 - Média abaixo de 5.0: Reprovado
@@ -7,3 +7,14 @@ final, de acordo com a média atingida:
 - Média de 7.0 ou superior: Aprovado
 '''
 
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+if media < 5:
+    print(f'Media {media:.1f}\nReprovado.')
+elif media == 5 or media <= 6.9:
+    print(f'Media {media:.1f}\nRecuperação.')
+elif media >= 7:
+    print(f'Media {media:.1f}\nAprovado.')
