0bfb4ac01ea420e7ee52003cffab73968bcc9f7a
marcosmorandi <marcosamorandi@gmail.com>
2023-06-20 16:03:41 -0300

    d042

--- desafios/d042.py
+++ desafios/d042.py
@@ -1,5 +1,5 @@
 '''
-Desafio 042
+Desafio 042 - Feito? Corrigir!
 Refaça o desafio 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo
 será formado:
 - Equilátero: Todos os lado iguais
@@ -7,3 +7,17 @@ será formado:
 - Escaleno: Todos os lados diferentes
 '''
 
+reta_1 = float(input('Digite o tamanho da primeira reta: '))
+reta_2 = float(input('Digite o tamanho da segunda reta: '))
+reta_3 = float(input('Digite o tamanho da terceira reta: '))
+
+if ((reta_1 + reta_2) > reta_3) and ((reta_1 + reta_3) > reta_2) and ((reta_2 + reta_3) > reta_1):
+    print('Essas medidas podem formar um triângulo.')
+    if reta_1 == reta_2 and reta_2 == reta_3 and reta_1 == reta_3:
+        print('Essas medidas formam um triângulo equilátero, com todos lados iguais.')
+    elif reta_1 != reta_2 and reta_2 != reta_3 and reta_1 != reta_3:
+        print('Essas medidas formam um triângulo escaleno, com todos os lados diferentes.')
+    else:
+        print('Essas medidas formam um triângulo isósceles, com dois lados iguas.')
+else:
+    print('Essas medidas não podem formar um triângulo.')
