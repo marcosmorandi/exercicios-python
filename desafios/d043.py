e207b3792e85ff6376c6fc66a3787cf0db027948
marcosmorandi <marcosamorandi@gmail.com>
2023-06-25 01:03:43 -0300

    d043

--- desafios/d043.py
+++ desafios/d043.py
@@ -1,5 +1,5 @@
 '''
-Desafio 043
+Desafio 043 - Feito
 Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu
 status, de acordo com a tabela abaixo:
 - Abaixo de 18.5: Abaixo do peso
@@ -8,3 +8,20 @@ status, de acordo com a tabela abaixo:
 - 30 até 40: Obesidade mórbida
 '''
 
+altura = float(input('Digite a altura: '))
+peso = float(input('Digite o peso: '))
+
+imc = peso / (altura * altura)
+
+print(f'IMC {imc:.2f}') # Com ":.1f" o "print" dos "elif" não funcionam.
+
+if imc < 18.5:
+    print('Abaixo do peso.')
+elif imc >= 18.5 and imc <= 25.99: # Com uma casa decimal o "print" não funciona nos "elif".
+    print('Peso ideal.')
+elif imc >= 26 and imc <= 30.99:
+    print('Sobrepeso')
+elif imc >= 31 and imc <= 40.99:
+    print('Obesidade')
+elif imc >= 41:
+    print('Obesidade mórbida')
