f737286c0157796cf7da93c5bae9377fd088500f
marcosmorandi <marcosamorandi@gmail.com>
2023-06-29 16:07:41 -0300

    e041

---
+++ exercicios/e041.py
@@ -0,0 +1,11 @@
+'''
+Desafio 041 - Feito
+A confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um
+atleta e mostre sua categoria, de acordo com a idade:
+- Até 9 anos: Mirin
+- Até 14 anos: Infantil
+- Até 19 anos: Junior
+- Até 20 anos: Sênior
+- Acima: Master
+'''
+
