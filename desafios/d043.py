2214961742b72cad65c5ae9043b0b60e5c1994b2
marcosmorandi <marcosamorandi@gmail.com>
2023-07-13 15:19:51 -0300

    d043

--- desafios/d043.py
+++ desafios/d043.py
@@ -5,7 +5,8 @@ status, de acordo com a tabela abaixo:
 - Abaixo de 18.5: Abaixo do peso
 - Entre 18.5 e 25: Peso ideal
 - 25 até 30: Sobrepeso
-- 30 até 40: Obesidade mórbida
+- 30 até 40: Obesidade
+- Acima de 40: Obesidade mórbida
 '''
 
 altura = float(input('Digite a altura: '))
