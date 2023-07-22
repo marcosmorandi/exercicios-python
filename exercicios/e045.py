1d1f10e85727ed98b11d4bcaebeb3c666bff1b89
marcosmorandi <marcosamorandi@gmail.com>
2023-07-22 16:44:31 -0300

    e045

--- exercicios/e045.py
+++ exercicios/e045.py
@@ -1,5 +1,50 @@
 '''
-Exercício 045
+Exercício 045 - Feito
 Crie um programa que faça o computador jogar Jokempô(pedra, papel, tesoura) com você.
 '''
 
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0,2)
+print('''Suas opções:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+print('-=' * 11)
+print(f'Computador jogou {itens[computador]}')
+print(f'Jogador jogou {itens[jogador]}')
+print('-=' * 11)
+if computador == 0: # computador jogou PEDRA
+    if jogador == 0: # jogador jogou PEDRA
+        print('EMPATE')
+    elif jogador == 1: # jogador jogou PAPEL
+        print('JOGADOR VENCE')
+    elif jogador == 2: # jogador jogou TESOURA
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 1: # computador jogou PAPEL
+    if jogador == 0: # jogador jogou PEDRA
+        print('COMPUTADOR VENCE')
+    elif jogador == 1: # jogador jogou PAPEL
+        print('EMPATE')
+    elif jogador == 2: # jogador jogou TESOURA
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 2: # computador jogou TESOURA
+    if jogador == 0: # jogador jogou PEDRA
+        print('JOGADOR VENCE')
+    elif jogador == 1: # jogador jogou PAPEL
+        print('COMPUTADOR VENCE')
+    elif jogador == 2: # jogador jogou TESOURA
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA!')
