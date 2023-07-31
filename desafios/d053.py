f28cd88fe2171b535e346095d2c7453c3a932ba5
marcosmorandi <marcosamorandi@gmail.com>
2023-07-31 15:46:00 -0300

    d053

---
+++ desafios/d053.py
@@ -0,0 +1,6 @@
+'''
+Desafio 053
+Crie um programa que leia uma frase qualquer e diga se ela é palíndromo, desconsidere os espaços.
+Palíndromo: Frase que pode ser lida do inicio ao fim ou do fim ao inicio e fica igual, desconsiderando espaços.
+Ex: apos a sopa, a sacada da casa, o lobo ama o bolo, anotaram a data da maratona.
+'''
