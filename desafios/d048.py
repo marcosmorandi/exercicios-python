d5df0f96b6b2d0bcf3c8fa6b95c79c36d844d34e
marcosmorandi <marcosamorandi@gmail.com>
2023-09-10 00:29:01 -0300

    d048

--- desafios/d048.py
+++ desafios/d048.py
@@ -1,5 +1,14 @@
 '''
-Desafio 048
-Faça um programa que calcule a soma entre todos os números impares que são multiplos de três
-e que se encontram no intervalo de 1 até 500.
+Desafio 048 - Feito
+Faça um programa que calcule a soma entre todos os números impares que são múltiplos de 
+3 e que se encontram no intervalo de 1 até 500.
 '''
+
+# Resultado é 20.667
+
+soma = 0 # Variável soma começa zerada.
+
+for c in range(1, 501): # Intervalo de 1 a 500 (501 não será mostrado).
+    if c % 2 != 0 and c % 3 == 0: # Resto da divisão por 2 diferente de 0 é impar. Resto da divisão por 3 igual a 0 é múltiplo de 3.
+        soma += c # Se atende a condição acima então soma.
+print(f'A soma dos números ímpares múltiplos de 3 entre 1 e 500 é {soma}') # Imprime o resultado.
