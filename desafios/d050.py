997730dc3885a4784dcecdd12a109b9fbb3f5dd8
marcosmorandi <marcosamorandi@gmail.com>
2023-09-14 14:59:57 -0300

    d050

--- desafios/d050.py
+++ desafios/d050.py
@@ -3,3 +3,9 @@ Desafio 050
 Desenvolva uma programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 Se o valor digitado for ímpar, desconsidere-o.
 '''
+
+
+for c in range(1, 7):
+    n = int(input('Digite um número inteiro: '))
+    print(c)
+print('Fim')
