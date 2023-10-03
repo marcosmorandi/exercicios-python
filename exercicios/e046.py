d5f2cd91d8e17256a42f5a047f152fb003e8cc75
marcosmorandi <marcosamorandi@gmail.com>
2023-10-03 15:29:12 -0300

    e046

--- exercicios/e046.py
+++ exercicios/e046.py
@@ -1,5 +1,11 @@
 '''
-Exercício 046
+Exercício 046 - Feito
 Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
 indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 '''
+
+from time import sleep
+for cont in range(10 ,-1, -1): # "cont" é a variável de controle. Os dois primeiros números são a contagem, o terceiro é o passo.
+    print(cont)
+    sleep(1)
+print('BUM! BUM! POOW!')
