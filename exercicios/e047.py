b80cd6cb3bb516043095a3a0b080e2632d92a8d3
marcosmorandi <marcosamorandi@gmail.com>
2023-10-06 16:19:08 -0300

    e047

--- exercicios/e047.py
+++ exercicios/e047.py
@@ -1,4 +1,19 @@
 '''
-Exercício 047
+Exercício 047 - Feito
 Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+'''
+
+# É sempre preferível fazer algoritmos que exijam menos das máquinas.
+for n in range(2, 51, 2):
+    print('.', end='') # Aqui mostra a quantidade de laços de repetição, 1, metade da solução abaixo, o que economiza processamento.
+    print(n, end=' ')
+print('Acabou')
+
+# A solução abaixo também funciona, mas será feito 2 laços de repetição para cada resiltado, aumentando o processamento.
+'''
+for n in range(1, 51):
+    print('.', end='') # Mosta os laços de repetição, nesse caso 2.
+    if n % 2 == 0:
+        print(n, end=' ')
+print('Acabou')
 '''
