e01fb068b665ac36dbd12b9247aff529be7050a1
marcosmorandi <marcosamorandi@gmail.com>
2023-10-06 16:22:18 -0300

    e047

--- exercicios/e047.py
+++ exercicios/e047.py
@@ -6,7 +6,7 @@ Crie um programa que mostre na tela todos os números pares que estão no interv
 # É sempre preferível fazer algoritmos que exijam menos das máquinas.
 for n in range(2, 51, 2):
     print('.', end='') # Aqui mostra a quantidade de laços de repetição, 1, metade da solução abaixo, o que economiza processamento.
-    print(n, end=' ')
+    print(n, end=' ') # O "end=''" serve para deixar tudo na mesa linha.
 print('Acabou')
 
 # A solução abaixo também funciona, mas será feito 2 laços de repetição para cada resiltado, aumentando o processamento.
@@ -14,6 +14,6 @@ print('Acabou')
 for n in range(1, 51):
     print('.', end='') # Mosta os laços de repetição, nesse caso 2.
     if n % 2 == 0:
-        print(n, end=' ')
+        print(n, end=' ') # O "end=''" serve para deixar tudo na mesa linha.
 print('Acabou')
 '''
