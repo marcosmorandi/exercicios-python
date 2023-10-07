739dc1dd96eb84857ed7bb9849893cb4bd3609b2
marcosmorandi <marcosamorandi@gmail.com>
2023-10-07 20:41:29 -0300

    d054

--- desafios/d054.py
+++ desafios/d054.py
@@ -4,5 +4,5 @@ Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre 
 ainda não atingiram a maioridade e quantas já são maiores.
 '''
 
-for c in range(1, 7):
-    ano_nascimento = input(int('Digite o ano de nascimento: '))
+for c in range(1, 8):
+    ano_nascimento = int(input('Digite o ano de nascimento: '))
