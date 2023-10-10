1b200d46c0c278dafd45e280ef01d61ebee52afd
marcosmorandi <marcosamorandi@gmail.com>
2023-10-10 15:52:41 -0300

    d055

--- desafios/d055.py
+++ desafios/d055.py
@@ -4,6 +4,8 @@ Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o 
 peso lido.
 '''
 
+maior_peso = 0
+menor_peso = 0
 for c in range(1, 6):
     peso = float(input(f'Digite o peso da pessoa {c}: '))
     
