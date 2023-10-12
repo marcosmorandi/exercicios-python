12a500777dcecdc81021c8ddfc8f12f5b2e79325
marcosmorandi <marcosamorandi@gmail.com>
2023-10-12 20:27:51 -0300

    e048

--- exercicios/e048.py
+++ exercicios/e048.py
@@ -1,5 +1,5 @@
 '''
-Desafio 048
+Desafio 048 - Feito
 Faça um programa que calcule a soma entre todos os números impares que são multiplos de três
 e que se encontram no intervalo de 1 até 500.
 '''
