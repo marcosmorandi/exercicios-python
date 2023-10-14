02b972a82525020a69975c522de0ca14e6e0b2cb
marcosmorandi <marcosamorandi@gmail.com>
2023-10-14 00:02:17 -0300

    e050

--- exercicios/e050.py
+++ exercicios/e050.py
@@ -1,5 +1,14 @@
 '''
-Exercício 050
+Exercício 050 - Feito
 Desenvolva uma programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 Se o valor digitado for ímpar, desconsidere-o.
 '''
+
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input(f'Digite o {c}º valor: '))
+    if num % 2 == 0:
+        soma = soma + num
+        cont = cont + 1
+print(f'Você informou {cont} números pares e a soma foi {soma}.')
