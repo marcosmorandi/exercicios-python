a0e1080bfc21d6117879d13e20eb726ceb875e2e
marcosmorandi <marcosamorandi@gmail.com>
2023-10-15 20:48:08 -0300

    e051

--- exercicios/e051.py
+++ exercicios/e051.py
@@ -1,5 +1,12 @@
 '''
-Exercício 051
+Exercício 051 - Feito
 Desenvolva um programa que leia o primeiro termo e a razão de uma PA(progressão aritmética).
 No final, mostre os 10 primeiros termos dessa progressão.
 '''
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 -1) * razao # Fórmula do enésimo termo de uma PA.
+for c in range(primeiro, decimo + razao, razao):
+    print(f'{c}', end=' -> ')
+print('Acabou')
