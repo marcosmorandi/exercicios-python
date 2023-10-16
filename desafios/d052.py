9a9f0ac04de7aa599b125200dacf58502e8be002
marcosmorandi <marcosamorandi@gmail.com>
2023-10-16 15:50:12 -0300

    d052

--- desafios/d052.py
+++ desafios/d052.py
@@ -1,26 +1,20 @@
 '''
-Desafio 052 - !Bug! Do 29 em diante ele não reconhece como primo.
+Desafio 052 - Feito
 Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 Número primo: Divisível apenas por 1 e por ele mesmo)
 '''
 
-def primo(numero):
-    if numero <= 1:
-        return False
-    elif numero <= 3:
-        return True
-    elif numero % 2 == 0 or numero % 3 == 0:
-        return False
-    for c in range(5, int(numero ** 0.5) + 1, 6):
-        if numero % 1 == 0 or numero % (c + 2) == 0:
-            return False
-    return True
-
-try:
-    numero = int(input('Digite um número inteiro: '))
-    if primo(numero):
-        print(f'{numero} é um número primo.')
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='') # Amarelo se divisível.
+        tot += 1 # Versão simplificada de "tot = tot + 1".
     else:
-        print(f'{numero} não é um número primo.')
-except ValueError:
-    print('Por favor, insira um número inteiro válido.')
+        print('\033[31m', end='') # Vermelho se não divisível.
+    print(f'{c}', end=' ') # Imprime na mesma linha todos os números do laço de repetição.
+print(f'\n\033[mO número {num} foi divisível {tot} vezes.') # "\n" para quebrar a linha e "\033[m" para voltar a cor branca padrão.
+if tot == 2:
+    print('E por isso ele é PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
