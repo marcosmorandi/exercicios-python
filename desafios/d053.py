06d7a7a80bb047dfbcffe7e14a7b22e0c78b7ca8
marcosmorandi <marcosamorandi@gmail.com>
2023-10-21 12:22:13 -0300

    d053

--- desafios/d053.py
+++ desafios/d053.py
@@ -1,5 +1,5 @@
 '''
-Desafio 053
+Desafio 053 - Feito
 Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando
 os espaços.
 Ex: "apos a sopa", "a sacada da casa", "o lobo ama o bolo", "anotaram a data da maratona".
