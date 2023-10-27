9786d0e06b7cee9f05c3b335438c99b8dc01fff1
marcosmorandi <marcosamorandi@gmail.com>
2023-10-27 14:27:18 -0300

    d055

--- desafios/d055.py
+++ desafios/d055.py
@@ -1,13 +1,20 @@
 '''
-Desafio 055 * melhorar o nome das variáveis
+Desafio 055 - Feito
 Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor
 peso lido.
 '''
 
 maior_peso = 0
 menor_peso = 0
-for c in range(1, 6):
-    peso = float(input(f'Digite o peso da pessoa {c}: '))
-    if peso > peso:
+for pessoa in range(1, 6):
+    peso = float(input(f'Digite o peso da pessoa {pessoa}: '))
+    if pessoa == 1:
         maior_peso = peso
-print(f'O maior peso foi {peso}.')
+        menor_peso = peso
+    else:
+        if peso > maior_peso:
+            maior_peso = peso
+        if peso < menor_peso:
+            menor_peso = peso
+print(f'O maior peso foi {maior_peso}Kg.')
+print(f'O menor peso foi {menor_peso}Kg.')
