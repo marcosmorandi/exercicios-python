23e2b65ad3efbee04561d1aeeefdde5280ae2668
marcosmorandi <marcosamorandi@gmail.com>
2023-11-09 16:04:42 -0300

    d058

---
+++ desafios/d058.py
@@ -0,0 +1,6 @@
+'''
+Desafio 058
+Melhore o jogo do desafio 028 onde o computador vai "pensar" em um número entre 0 e 10.
+Só que agora o jogador vai tentar advinhar até acertar, mostrando no final quantos palpites
+foram necessários para vencer.
+'''
