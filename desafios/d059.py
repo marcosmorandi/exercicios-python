0da77e3e38b62c5177ef720da021d45014438a83
marcosmorandi <marcosamorandi@gmail.com>
2023-11-13 00:53:33 -0300

    d059

--- desafios/d059.py
+++ desafios/d059.py
@@ -8,3 +8,34 @@ Crie um programa que leia dois valores e mostre um menu na tela:
 [5] sair do programa
 Seu programa deverá realizar a operação solicitada em cada caso.
 '''
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+operacao = 0
+while operacao == 1 or 2 or 3 or 4 or 5:
+    operacao = int(input('Escolha a operação:\n[1] somar\n[2] multiplicar\n[3] maior\n[4] novos números\n[5] sair do programa\nSua escolha: '))
+
+    if operacao == 1:
+        soma = n1 + n2
+        print(f'O resultado da soma de {n1} e {n2} é {soma}.')
+
+    elif operacao == 2:
+        multiplicacao = n1 * n2
+        print(f'O resultado da multiplicação de {n1} e {n2} é {multiplicacao}.')
+
+    elif operacao == 3:
+        if n1 > n2:
+            print(f'O primeiro valor ({n1}) é o maior.')
+        elif n2 > n1:
+            print(f'O segundo valor ({n2}) é o maior.')
+        else:
+            print('Ambos valores são iguais')
+# Aqui pra baixo não funciona
+    elif operacao == 4:
+        n1 = int(input('Digite o primeiro valor: '))
+        n2 = int(input('Digite o segundo valor: '))
+        operacao = 0
+        operacao = int(input('Escolha a operação:\n[1] somar\n[2] multiplicar\n[3] maior\n[4] novos números\n[5] sair do programa\nSua escolha: '))
+
+    elif operacao == 5:
+        print('Você escolheu sair!')
