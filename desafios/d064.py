3a6c2075bd87c246024b532441ecd8742ebee8cc
marcosmorandi <marcosamorandi@gmail.com>
2023-11-18 02:50:31 -0300

    d064

--- desafios/d064.py
+++ desafios/d064.py
@@ -1,1 +1,6 @@
-
+'''
+Desafio 064
+Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando
+o usuário digitar o valor 999, que é o condição de parada.
+No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
+'''
