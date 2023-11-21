7301dcd48884270152f582deb77cf2f1d3c54f68
marcosmorandi <marcosamorandi@gmail.com>
2023-11-21 13:33:48 -0300

    d057

--- exercicios/e057.py
+++ exercicios/e057.py
@@ -7,4 +7,4 @@ Caso esteja errado, peça a digitação novamente até ter um valor correto.
 sexo = str(input('Infome o seu sexo [M/F]: ')).strip().upper()[0] # "strip" elimina possíveis espaços vazios, "upper" deixa tudo maiúscula, "[0]" pega só a primeira letra.
 while sexo not in 'MmFf': # "enquanto não for...", com o "upper" nem precisaria testar as minúsculas, mas assim deixa o código mais legível.
     sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
-print(f'Sexo {sexo} registrado com sucesso')
+print(f'Sexo {sexo} registrado com sucesso.')
