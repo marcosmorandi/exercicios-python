b1dbe6deb66be87078afd0bc8db0b97e5a1ab2b6
marcosmorandi <marcosamorandi@gmail.com>
2023-11-24 15:28:53 -0300

    e058

--- exercicios/e058.py
+++ exercicios/e058.py
@@ -1,5 +1,5 @@
 '''
-Exercício 058
+Exercício 058 - Feito
 Melhore o jogo do desafio 028 onde o computador vai "pensar" em um número entre 0 e 10.
 Só que agora o jogador vai tentar advinhar até acertar, mostrando no final quantos palpites
 foram necessários para vencer.
@@ -24,3 +24,14 @@ print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
 print('Será que você consegue advinhar qual foi?')
 acertou = False # Variável booleana que recebe "True" ou "False" para verdadeiro ou falso.
 palpites = 0
+while not acertou: # O laço de repetição será feito enquanto não acertar, ou seja enquanto o "acertou" continuar "False".
+    jogador = int(input('Qual é o seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True # Se cair nesse "if", aqui acaba o laço de repetição.
+    else: # Senão aqui continua.
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print(f'Acertou com {palpites} tentativas. Parabéns!')
