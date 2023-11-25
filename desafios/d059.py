4b8c907f1cae4269ec25f711937883c3aa3bec26
marcosmorandi <marcosamorandi@gmail.com>
2023-11-25 18:16:21 -0300

    d059

--- desafios/d059.py
+++ desafios/d059.py
@@ -1,5 +1,5 @@
 '''
-Desafio 059
+Desafio 059 - Feito
 Crie um programa que leia dois valores e mostre um menu na tela:
 [1] somar
 [2] multiplicar
@@ -12,16 +12,18 @@ Seu programa deverá realizar a operação solicitada em cada caso.
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
 operacao = 0
-while operacao == 1 or 2 or 3 or 4 or 5:
+while operacao != 5:
+    print('-' * 50) # Muito melhor do que digitar 50 vezes o caracter "-".
     operacao = int(input('Escolha a operação:\n[1] somar\n[2] multiplicar\n[3] maior\n[4] novos números\n[5] sair do programa\nSua escolha: '))
+    print('-' * 50)
 
     if operacao == 1:
         soma = n1 + n2
-        print(f'O resultado da soma de {n1} e {n2} é {soma}.')
+        print(f'O resultado da soma de {n1} + {n2} é {soma}.')
 
     elif operacao == 2:
         multiplicacao = n1 * n2
-        print(f'O resultado da multiplicação de {n1} e {n2} é {multiplicacao}.')
+        print(f'O resultado da multiplicação de {n1} X {n2} é {multiplicacao}.')
 
     elif operacao == 3:
         if n1 > n2:
@@ -30,12 +32,15 @@ while operacao == 1 or 2 or 3 or 4 or 5:
             print(f'O segundo valor ({n2}) é o maior.')
         else:
             print('Ambos valores são iguais')
-# Aqui pra baixo não funciona
+
     elif operacao == 4:
         n1 = int(input('Digite o primeiro valor: '))
         n2 = int(input('Digite o segundo valor: '))
-        operacao = 0
-        operacao = int(input('Escolha a operação:\n[1] somar\n[2] multiplicar\n[3] maior\n[4] novos números\n[5] sair do programa\nSua escolha: '))
 
     elif operacao == 5:
         print('Você escolheu sair!')
+
+    else:
+        print('Opção inválida, tente novamente!')
+
+print('Fim do programa.')
