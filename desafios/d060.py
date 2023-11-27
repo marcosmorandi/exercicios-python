0276aa59e075c4c2d3cc53599788dc70b04bff4b
marcosmorandi <marcosamorandi@gmail.com>
2023-11-27 00:08:50 -0300

    d060

--- desafios/d060.py
+++ desafios/d060.py
@@ -1,5 +1,16 @@
 '''
-Desafio 060
+Desafio 060 - Feito
 Faça um programa que leia um número qualquer e mostre o seu fatorial.
 EX: 5!=5x4x3x2x1=120
 '''
+
+numero = int(input('Digite um número para calcular seu fatorial: '))
+contador = numero
+fatorial = 1 # Fator nulo de multiplicação é 1. Nulo de soma ou subtração é 0.
+print(f'Calculando {numero} ! = ', end='') # "end=''" para não pular linha.
+while contador > 0:
+    print(f'{contador}', end='')
+    print(' x ' if contador > 1 else ' = ', end='') # É possível usar "if", "else" dentro de um "print".
+    fatorial *= contador
+    contador -= 1
+print(f'{fatorial}')
