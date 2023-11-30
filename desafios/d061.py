f2b4b830b711570b4abd4ac86dbdb5378c5fbf9d
marcosmorandi <marcosamorandi@gmail.com>
2023-11-30 15:48:07 -0300

    d061

--- desafios/d061.py
+++ desafios/d061.py
@@ -24,10 +24,10 @@ print('-=-' * 10)
 primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão da PA: '))
 termo = primeiro
-cont = 1
+contador = 1
 print('Os 10 primeiros termos dessa PA são:')
-while cont <= 10:
+while contador <= 10:
     print(f'{termo} -> ', end='')
     termo += razao
-    cont += 1
+    contador += 1
 print('FIM')
