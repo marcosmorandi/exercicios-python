1ce3c186a68bedc4314878f1da1a4f6463e94495
marcosmorandi <marcosamorandi@gmail.com>
2023-12-02 14:59:56 -0300

    d062

--- desafios/d062.py
+++ desafios/d062.py
@@ -3,3 +3,23 @@ Desafio 062
 Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos.
 O programa encerra quando ele disser que quer mostrar 0 termos.
 '''
+
+# Desafio 061 - Feito
+# Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos
+# da progressão usando a estrutura "while".
+
+# print('-=-' * 10)
+# print('Gerador de PA')
+# print('-=-' * 10)
+
+# primeiro = int(input('Primeiro termo: '))
+# razao = int(input('Razão da PA: '))
+# termo = primeiro
+# contador = 1
+# print('Os 10 primeiros termos dessa PA são:')
+# while contador <= 10:
+#     print(f'{termo} -> ', end='')
+#     termo += razao
+#     contador += 1
+# print('FIM')
+
