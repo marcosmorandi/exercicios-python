0ed861c658f6f0a443ff060f0cbae896284938af
marcosmorandi <marcosamorandi@gmail.com>
2023-12-06 12:15:26 -0300

    e062

--- exercicios/e062.py
+++ exercicios/e062.py
@@ -1,5 +1,5 @@
 '''
-Exercício 062
+Exercício 062 - Feito
 Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos.
 O programa encerra quando ele disser que quer mostrar 0 termos.
 '''
@@ -24,3 +24,22 @@ O programa encerra quando ele disser que quer mostrar 0 termos.
 #     contador += 1
 # print('FIM')
 
+print('-=-' * 10)
+print('Gerador de PA')
+print('-=-' * 10)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+contador = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while contador <= total:
+        print(f'{termo} -> ', end='')
+        termo += razao
+        contador += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print(f'Progressão finalizada com {total} termos mostrados.')
