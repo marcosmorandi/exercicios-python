35ebdde4f09ac0b7b1022ac0556301a489a972d5
marcosmorandi <marcosamorandi@gmail.com>
2023-12-08 15:42:21 -0300

    d063

--- desafios/d063.py
+++ desafios/d063.py
@@ -1,7 +1,18 @@
 '''
-Desafio 063
+Desafio 063 - Feito
 Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos
 de uma Sequência de Fibonacci.
 Ex:
 0 > 1 > 1 > 2 > 3 > 5 > 8
 '''
+
+numero = int(input('Digite um número inteiro para a Sequência de Fibonacci: '))
+
+a, b = 0, 1
+
+contador = 0
+
+while contador < numero:
+    print(a, end=' ')
+    a, b = b, a + b
+    contador += 1
