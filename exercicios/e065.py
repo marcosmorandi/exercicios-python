f2e1bfbb27054c32f622e9058558a78797a3f1b0
marcosmorandi <marcosamorandi@gmail.com>
2023-12-15 16:12:19 -0300

    e065

--- exercicios/e065.py
+++ exercicios/e065.py
@@ -4,3 +4,5 @@ Crie um programa que leia vários números inteiros pelo teclado. No final da ex
 média entre todos os valores e qual foi o maior e o menor valor lido.
 O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 '''
+
+numero = int(input('Digite um número inteiro: '))
