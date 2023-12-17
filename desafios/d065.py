95de08a56a8a8cc8043727dc0b9b387f26586007
marcosmorandi <marcosamorandi@gmail.com>
2023-12-16 21:48:03 -0300

    d065

--- desafios/d065.py
+++ desafios/d065.py
@@ -1,8 +1,25 @@
 '''
-Desafio 065
-Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a 
+Desafio 065 - Feito
+Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a
 média entre todos os valores e qual foi o maior e o menor valor lido.
 O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 '''
 
-numero = int(input('Digite um número inteiro: '))
+novo_valor = 's'
+numero = 0
+media = 0
+maior = 0
+menor = 9999
+cont = 0
+soma = 0
+while novo_valor == 's':
+    numero = int(input('Digite um número inteiro: '))
+    cont += 1
+    if numero > maior:
+        maior = numero
+    if numero < menor:
+        menor = numero
+    soma += numero
+    media = soma / cont
+    novo_valor = str(input('Quer continuar digitando valores [S/N]: ')).lower()
+print(f'Foram digitados {cont} números. O maior valor digitado foi {maior} o menor valor digitado foi {menor} e a média é {media}.')
