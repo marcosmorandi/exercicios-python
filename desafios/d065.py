b661303bee2ecd201e5c8ceebd0b215a58c7b37b
marcosmorandi <marcosamorandi@gmail.com>
2023-12-18 00:56:35 -0300

    d065

--- desafios/d065.py
+++ desafios/d065.py
@@ -5,21 +5,20 @@ média entre todos os valores e qual foi o maior e o menor valor lido.
 O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 '''
 
-novo_valor = 's'
-numero = 0
-media = 0
-maior = 0
-menor = 9999
-cont = 0
-soma = 0
-while novo_valor == 's':
+valor = 's'
+soma = contador = media = maior = menor = 0 # Quando várias variáveis começam com o mesmo número, 0 no caso, pode se colocar na mesma linha.
+while valor in 'Ss': # Enqunto o valor estiver em S maiúsculo ou minúsculo.
     numero = int(input('Digite um número inteiro: '))
-    cont += 1
-    if numero > maior:
-        maior = numero
-    if numero < menor:
-        menor = numero
+    contador += 1
     soma += numero
-    media = soma / cont
-    novo_valor = str(input('Quer continuar digitando valores [S/N]: ')).lower()
-print(f'Foram digitados {cont} números. O maior valor digitado foi {maior} o menor valor digitado foi {menor} e a média é {media}.')
+    if contador == 1:
+        maior = menor = numero # Se for digitado apenas um número, o maior e menor serão esse número.
+    else: # Depois do primeiro numero ele começa a comparar para colocar o maior e menor nas suas respectivas posições.
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+    media = soma / contador # O cálculo da média funciona dentro ou fora do "while".
+    valor = str(input('Quer continuar digitando valores [S/N]: ')).upper().strip()[0] # Joga para maiúsculas, remove espaços e só considera a primeira letra.
+print(f'Foram digitados {contador} número(s).')
+print(f'O maior valor digitado foi {maior}, o menor valor digitado foi {menor} e a média entre ele é {media}.')
