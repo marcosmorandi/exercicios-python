a65f67c931452997f7a2b597cacdcfda1f35ab3b
marcosmorandi <marcosamorandi@gmail.com>
2023-12-20 15:12:50 -0300

    e065

--- exercicios/e065.py
+++ exercicios/e065.py
@@ -1,6 +1,6 @@
 '''
 Exercício 065 - Feito
-Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a 
+Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a
 média entre todos os valores e qual foi o maior e o menor valor lido.
 O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 '''
@@ -11,7 +11,7 @@ while resp in 'Ss': # Quando várias variáveis começam com o mesmo número, 0 
     num = int(input('Digite um número: '))
     soma += num
     quant += 1
-    if quant == 1: 
+    if quant == 1:
         maior = menor = num # Se for digitado apenas um número, o maior e menor serão esse número.
     else: # Depois do primeiro numero ele começa a comparar para colocar o maior e menor nas suas respectivas posições.
         if num > maior:
