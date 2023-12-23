17f2cf5be96d0c65c8af80a8d67ef97b0d0bed0d
marcosmorandi <marcosamorandi@gmail.com>
2023-12-23 19:18:45 -0300

    d066

--- desafios/d066.py
+++ desafios/d066.py
@@ -5,5 +5,6 @@ usuário digitar o valor 999, que é a condição de parada.
 No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
 '''
 
+# Exemplo:
 # Digite um valor (999 para parar):
 # A soma dos x valores foi x!
