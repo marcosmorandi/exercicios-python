24d32b3a09f52a9d2663d7762c899d1e0319be4c
marcosmorandi <marcosamorandi@gmail.com>
2023-12-24 19:56:07 -0300

    d067

--- desafios/d067.py
+++ desafios/d067.py
@@ -1,1 +1,16 @@
+'''
+Desafio 067
+Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado
+pelo usuário.
+O programa será interrompido quando o número solicitado for negativo.
+'''
 
+# Exemplo:
+# ---------------------------------
+# Quer ver a tabua de que valor: x
+# ---------------------------------
+# (mostra tabuada)
+# ---------------------------------
+# Quer ver a tabua de que valor: -x
+# ---------------------------------
+# Programa tabuada encerrado. Volte sempre!
