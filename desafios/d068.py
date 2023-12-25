025444a0a89de3f981b0ddd1456d4b93138cae28
marcosmorandi <marcosamorandi@gmail.com>
2023-12-25 15:27:14 -0300

    d068

--- desafios/d068.py
+++ desafios/d068.py
@@ -1,1 +1,32 @@
+'''
+Desafio 068
+Faça um programa que jogue par ou ímpar com o computador.
+O jogo será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que
+ele conquistou no final do jogo.
+'''
 
+# Exemplo:
+# =-=-=-=-=-=-=-=-=-=-=-=-=
+# Vamos jogar par ou ímpar
+# =-=-=-=-=-=-=-=-=-=-=-=-=
+# Diga um valor: 8
+# Par ou Ímpar? [P/I] p
+# -------------------------
+# Voce jogou 8 e o computador 2. Total de 16, DEU PAR
+# -------------------------
+# Você VENCEU!
+# Vamos jogar novamente...
+# =-=-=-=-=-=-=-=-=-=-=-=-=
+# Diga um valor: 8
+# ou
+# =-=-=-=-=-=-=-=-=-=-=-=-=
+# Vamos jogar par ou ímpar
+# =-=-=-=-=-=-=-=-=-=-=-=-=
+# Diga um valor: 6
+# Par ou Ímpar? [P/I] i
+# -------------------------
+# Voce jogou 6 e o computador 10. Total de 16, DEU PAR
+# -------------------------
+# Você PERDEU!
+# =-=-=-=-=-=-=-=-=-=-=-=-=
+# GAME OVER! Você venceu 0 vezes.
