8ad690704efeae496052401c1b1b6eec77f251a3
marcosmorandi <marcosamorandi@gmail.com>
2023-12-26 00:09:09 -0300

    d069

--- desafios/d069.py
+++ desafios/d069.py
@@ -1,1 +1,38 @@
+'''
+Desafio 069 
+Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa
+deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+A) Quantas pessoas tem mais de 18 anos.
+B) Quantos homens foram cadastrados.
+C) Quantas mulheres tem menos de 20 anos.
+'''
 
+# Exemplo:
+# ---------------------
+#  Cadastre Uma Pessoa
+# ---------------------
+# Idade: 33
+# Sexo [M/F]: M
+# -------------------
+# Quer continar [S/N]: S
+# ---------------------
+#  Cadastre Uma Pessoa
+# ---------------------
+# Idade: 25
+# Sexo [M/F]: g
+# Sexo [M/F]: y
+# Sexo [M/F]: f
+# -------------------
+# Quer continar [S/N]: w
+# Quer continar [S/N]: s
+# ---------------------
+#  Cadastre Uma Pessoa
+# ---------------------
+# Idade: 8
+# Sexo [M/F]: m
+# ---------------------
+# Quer continar [S/N]: n
+# ========== Fim do Programa ==========
+# Total de pessoas com mais de 18 anos: 2
+# Ao todo temos 2 homens cadastrados
+# E temos 1 mulher(es) com menos de 20 anos
