23ae2b5943edf37782381b60302705cdf2d91fa2
marcosmorandi <marcosamorandi@gmail.com>
2023-12-27 19:13:42 -0300

    d070

--- desafios/d070.py
+++ desafios/d070.py
@@ -1,1 +1,23 @@
+'''
+Desafio 070
+Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o 
+usuário vai continuar. No final, mostre:
+A) Qual é o total gasto na compra.
+B) Quantos produtos custam mais de R$ 1000,00.
+C) Qual é o nome do produto mais barato.
+'''
 
+# --------------------
+#  Loja Super Baratão
+# --------------------
+# Nome do produto: Mouse
+# Preço: R$ 50
+# Quer continuar? [S/N] y
+# Quer continuar? [S/N] s
+# Nome do produto: Caneta
+# Preço: R$ 3
+# Quer continuar? [S/N] n
+# ----- Fim do Programa -----
+# O total da compra foi R$ 53.00
+# Temos 0 produto(s) custando mais de R$ 1000.00
+# O produto mais barato foi Caneta que custa R$ 3.00
