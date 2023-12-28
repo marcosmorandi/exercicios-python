1e4e97f91ce0182705e6ed78a6be67bc72536ca7
marcosmorandi <marcosamorandi@gmail.com>
2023-12-28 20:38:28 -0300

    d071

---
+++ desafios/d071.py
@@ -0,0 +1,19 @@
+'''
+Desafio 071
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usupario 
+qual será o valor sacado (número inteiro) e o programa vai informar quantas células de cada valor 
+serão entregues.
+OBS: Considere que o caixa possui células de R$ 50, R$ 20, R$ 10 e R$ 1.
+'''
+
+# Exemplo:
+# ========================
+#        Banco CEV        
+# ========================
+# Que valor você quer sacar: RS 1234
+# Total de 24 cédula(s) de R$ 50
+# Total de 1 cédula(s) de R$ 20
+# Total de 1 cédula(s) de R$ 10
+# Total de 4 cédula(s) de R$ 1
+# ========================
+# Volte sempre ao Banco CEV! Tenha um bom dia!
