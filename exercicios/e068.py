b5358496951b9a166cac1b43f0d5b85241f03359
marcosmorandi <marcosamorandi@gmail.com>
2023-12-31 17:32:01 -0300

    e068

--- exercicios/e068.py
+++ exercicios/e068.py
@@ -1,1 +1,6 @@
-
+'''
+Exercício 068
+Faça um programa que jogue par ou ímpar com o computador.
+O jogo será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que
+ele conquistou no final do jogo.
+'''
