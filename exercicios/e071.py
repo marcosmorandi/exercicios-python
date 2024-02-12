e429219bf7e0505553c532eb292b3b396cd6a908
marcosmorandi <marcosamorandi@gmail.com>
2024-02-12 15:29:22 -0300

    e071

---
+++ exercicios/e071.py
@@ -0,0 +1,7 @@
+'''
+Exercício 071
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usupario 
+qual será o valor sacado (número inteiro) e o programa vai informar quantas células de cada valor 
+serão entregues.
+OBS: Considere que o caixa possui células de R$ 50, R$ 20, R$ 10 e R$ 1.
+'''
