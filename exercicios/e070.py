98fde151aabec35869e175fa61e4478154115c0f
marcosmorandi <marcosamorandi@gmail.com>
2024-02-12 13:43:44 -0300

    070

---
+++ exercicios/e070.py
@@ -0,0 +1,9 @@
+'''
+Exercicio 070
+Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o 
+usuário vai continuar. No final, mostre:
+A) Qual é o total gasto na compra.
+B) Quantos produtos custam mais de R$ 1000,00.
+C) Qual é o nome do produto mais barato.
+'''
+
