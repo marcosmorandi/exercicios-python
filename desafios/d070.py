9edc97f17245df592f7cc7eff873fdcded2c6c17
marcosmorandi <marcosamorandi@gmail.com>
2024-02-12 15:26:25 -0300

    d070

--- desafios/d070.py
+++ desafios/d070.py
@@ -1,5 +1,5 @@
 '''
-Desafio 070 - Bug no produto mais barato
+Desafio 070 - Feito
 Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o 
 usuário vai continuar. No final, mostre:
 A) Qual é o total gasto na compra.
@@ -22,25 +22,27 @@ C) Qual é o nome do produto mais barato.
 # Temos 0 produto(s) custando mais de R$ 1000.00
 # O produto mais barato foi Caneta que custa R$ 3.00
 
-preco = total_compra = mais_mil = mais_barato = 0
+total_compra = mais_mil = v_mais_barato = contador = 0
+p_mais_barato = ' '
 print('-'*20)
 print(' Loja Super Baratão ')
 print('-'*20)
 while True:
     produto = str(input('Nome do produto: '))
     preco = float(input('Preço: R$ '))
-    continua = ' '
-    while continua not in 'SN':
-        continua = str(input('Quer continuar [S/N]? ')).strip().upper()[0]
+    contador += 1
     total_compra += preco
     if preco > 1000:
         mais_mil += 1
-    mais_barato == preco
-    if preco < mais_barato:
-        mais_barato = preco
+    if contador == 1 or preco < v_mais_barato:
+        v_mais_barato = preco
+        p_mais_barato = produto
+    continua = ' '
+    while continua not in 'SN':
+        continua = str(input('Quer continuar [S/N]? ')).strip().upper()[0]
     if continua == 'N':
         break
 print('-'*5 + ' Fim do Programa ' + '-'*5)
 print(f'O total da compra foi R$ {total_compra:.2f}')
 print(f'Temos {mais_mil} produto(s) custando mais de R$ 1000.00')
-print(f'O produto mais barato foi {produto} que custa R$ {preco:.2f}')
+print(f'O produto mais barato foi {p_mais_barato} que custa R$ {v_mais_barato:.2f}')
