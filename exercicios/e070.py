73b152bd30e0937e50f6d6f15de942b145d35896
marcosmorandi <marcosamorandi@gmail.com>
2024-02-12 14:05:22 -0300

    e070

--- exercicios/e070.py
+++ exercicios/e070.py
@@ -1,5 +1,5 @@
 '''
-Exercicio 070
+Exercicio 070 - Feito
 Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o 
 usuário vai continuar. No final, mostre:
 A) Qual é o total gasto na compra.
@@ -7,3 +7,24 @@ B) Quantos produtos custam mais de R$ 1000,00.
 C) Qual é o nome do produto mais barato.
 '''
 
+total = totmil = menor = cont = 0
+barato = ''
+while True:
+    produto = str(input('Nome do produto: '))
+    preco = float(input('Preço: R$ '))
+    cont += 1
+    total += preco
+    if preco > 1000:
+        totmil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'O total da compra foi R$ {total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {barato} que custa R$ {menor:.2f}')
