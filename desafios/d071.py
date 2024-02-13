57a118785bc680e58e9d93fdb19b625e5370ff1f
marcosmorandi <marcosamorandi@gmail.com>
2024-02-13 14:25:11 -0300

    d071

--- desafios/d071.py
+++ desafios/d071.py
@@ -17,3 +17,29 @@ OBS: Considere que o caixa possui células de R$ 50, R$ 20, R$ 10 e R$ 1.
 # Total de 4 cédula(s) de R$ 1
 # ========================
 # Volte sempre ao Banco CEV! Tenha um bom dia!
+
+print('='*30)
+print('{:^30}'.format('BANCO CEV'))
+print('='*30)
+valor = int(input('Que valor você quer sacar? R$ '))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R$ {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('='*30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
