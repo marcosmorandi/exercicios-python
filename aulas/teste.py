a7db953dcb129e2b99fd85763ebf84611b3f44c4
marcosmorandi <marcosamorandi@gmail.com>
2024-02-13 14:42:28 -0300

    teste

--- aulas/teste.py
+++ aulas/teste.py
@@ -1,7 +1,13 @@
 # Considere a string s = 'prova de python'. A instrução len(s) retornaria qual valor?
-s = 'prova de python'
-print(len(s))
+#s = 'prova de python'
+#print(len(s))
 
 # Qual é o resultado calculado pelo Python para a expressão composta 3 * 5 + 4 ** 2?
-resultado = 3 * 5 + 4 ** 2
-print(f'{resultado}')
+#resultado = 3 * 5 + 4 ** 2
+#print(f'{resultado}')
+
+
+# Se usarmos a estrutura for c in range(0, 10, 3): no Python e colocarmos dentro do bloco
+# um comando print(c), quais serão os valores impressos na tela do terminal?
+#for c in range(0, 10, 3):
+#    print(c)
