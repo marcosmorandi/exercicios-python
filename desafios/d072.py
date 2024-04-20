7b036eae3273aab08adc11bf93def1e4f3537293
marcosmorandi <marcosamorandi@gmail.com>
2024-04-20 13:38:58 -0300

    d072

---
+++ desafios/d072.py
@@ -0,0 +1,10 @@
+'''
+Desafio 072
+Crie um programa que tem uma tupla totalmente preenchida com uma contagem por extenso,
+do zero até vinte.
+Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostra-lo por extenso.
+Se colocar um numero menor que 0 ou maior que 20 tem que mostrar uma mensagem para digitar novamente.
+'''
+
+contagem = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quize', 'dezesseis', 'dezesste', 'dezoito', 'dezenove', 'vinte')
+numero = str(input('Digite o numero que quer mostrar por extenso, de 0 a 20: '))
