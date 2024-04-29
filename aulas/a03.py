3263e55a6277033a70ee250e863d9e06d88da515
marcosmorandi <marcosamorandi@gmail.com>
2024-04-29 00:40:42 -0300

    a03

---
+++ aulas/a03.py
@@ -0,0 +1,10 @@
+# The Zen of Python, by Tim Peters
+import this
+
+'''
+Verificar a versão do python instala na máquina:
+No terminal, "python --version" [enter]
+
+Entrar no interpretador de comandos e testar alguns comandos:
+No teminal, "python" [enter] + o comandado que quiser, exemplo "print('Hello word!')"
+'''
