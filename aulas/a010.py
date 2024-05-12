fd70da8b65860b2104aa11f511c8acf3eba6818a
marcosmorandi <marcosamorandi@gmail.com>
2024-05-12 13:54:31 -0300

    a010

---
+++ aulas/a010.py
@@ -0,0 +1,24 @@
+# Estrutura condicional.
+# Todo método tem parenteses no final.
+# "if:" = se; "else:" = senão.
+# Todo comando colado do lado esquerdo da tela será executado sempre. Todo comando com identação (para direita) pode ser executado ou não.
+
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é mais bonito que Gustavo!')
+print(f'Bom dia {(nome)}!')
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print(f'A sua média foi {(m):.1f}.')
+'''
+if m >= 6.0:
+    print('Sua média foi boa! Parabéns!')
+else:
+    print('Sua média foi ruim! Estude mais!')
+'''
+# Condição simplificada:
+print('Parabéns!' if m >= 6 else 'Estude Mais!')
