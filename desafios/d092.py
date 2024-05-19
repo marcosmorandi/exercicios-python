fcf8f0571c4adccca3d6508c5e5dfc48ed3da913
marcosmorandi <marcosamorandi@gmail.com>
2024-05-19 00:31:46 -0300

    d092

---
+++ desafios/d092.py
@@ -0,0 +1,7 @@
+'''
+Desafio 092
+Crie um programa que leia o nome, ano de nascimento e carteira de trabalho e cadastre-os
+(com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá
+também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos
+a pessoa vai se aposentar.
+'''
