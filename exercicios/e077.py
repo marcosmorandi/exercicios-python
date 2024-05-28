a92bd5a211b4787672c143b38af6c1965b8f7aa2
marcosmorandi <marcosamorandi@gmail.com>
2024-05-28 16:10:31 -0300

    e077

---
+++ exercicios/e077.py
@@ -0,0 +1,14 @@
+'''
+Exercício 077 - Feito
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+'''
+
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 
+            'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end=' ')
+    for letra in p:
+        if letra.lower() in 'aeiou': # Se a letra jogada para minúscula for dentro de "aeiou"...
+            print(letra, end=' ') # ... escreve a letra.
