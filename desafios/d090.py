bc5cc49643a38c8407a1b618fad4bc6d28555357
marcosmorandi <marcosamorandi@gmail.com>
2024-11-14 15:45:21 -0300

    d090

--- desafios/d090.py
+++ desafios/d090.py
@@ -1,5 +1,22 @@
 '''
-Desafio 090
+Desafio 090 - Feito
 Faça um programa que leia nome e média de um aluno, guardando também a situação de um dicionáiro.
 No final, mostre o conteúdo da estrutura na tela.
 '''
+
+aluno = dict()
+
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print('-=' * 30)
+
+for k, v, in aluno.items():
+    print(f' - {k} é igual a {v}')
